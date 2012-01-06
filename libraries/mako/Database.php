660206f9fe1b6a6a9956b7229ecdafb3c2688e55
Frederic G. Østby <frederic.g.ostby@gmail.com>
2012-01-06 16:29:06 +0100

    Updated database exceptions

--- libraries/mako/Database.php
+++ libraries/mako/Database.php
@@ -99,7 +99,7 @@ namespace mako
 				
 				if(isset(static::$config['configurations'][$name]) === false)
 				{
-					throw new RuntimeException(__CLASS__ . ": '{$name}' has not been defined in the database configuration.");
+					throw new RuntimeException(vsprintf("%s: '%s' has not been defined in the database configuration.", array(__CLASS__, $name)));
 				}
 				
 				static::connect($name);
@@ -137,7 +137,7 @@ namespace mako
 			}
 			catch(PDOException $e)
 			{
-				throw new RuntimeException(__CLASS__ . ": Failed to connect to the {$name} database.<br /><br />" . $e->getMessage());
+				throw new RuntimeException(vsprintf("%s: Failed to connect to the '%s' database.<br /><br />%s", array(__CLASS__, $name, $e->getMessage())));
 			}
 
 			// Run queries
