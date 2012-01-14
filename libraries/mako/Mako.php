67391f76b327864ace0745dae624d62c17110f55
Frederic G. Østby <frederic.g.ostby@gmail.com>
2012-01-14 18:32:44 +0100

    Fix for compile errors when highlighting the first line of a multi line comment

--- libraries/mako/Mako.php
+++ libraries/mako/Mako.php
@@ -346,10 +346,10 @@ namespace mako
 
 			$highlight = function($string)
 			{
-				$search  = array("\n", '<code>', '</code>', '<span style="color: #0000BB">&lt;?php&nbsp;');
-				$replace = array('', '', '', '<span style="color: #0000BB">');
+				$search  = array("\n", '<code>', '</code>', '<span style="color: #0000BB">&lt;?php&nbsp;', '#$@r4!/*');
+				$replace = array('', '', '', '<span style="color: #0000BB">', '/*');
 
-				return str_replace($search, $replace, highlight_string('<?php ' . $string, true));	
+				return str_replace($search, $replace, highlight_string('<?php ' . str_replace('/*', '#$@r4!/*', $string), true));	
 			};
 
 			$handle      = fopen($file, 'r');
