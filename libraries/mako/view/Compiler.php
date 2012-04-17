12ba6de1487f4c78ae595408e71d1bd7f6540377
Frederic G. Østby <frederic.g.ostby@gmail.com>
2012-04-17 10:44:13 +0200

    Tag matching is now case insensitive

--- libraries/mako/view/Compiler.php
+++ libraries/mako/view/Compiler.php
@@ -129,11 +129,11 @@ class Compiler
 	{
 		// Compile blocks
 
-		$template = preg_replace('/{%\s{0,}block:(.*?)\s{0,}%}(.*?){%\s{0,}endblock\s{0,}%}/s', '<?php mako\view\Block::open(\'$1\'); ?>$2<?php mako\view\Block::close(); ?>', $template);
+		$template = preg_replace('/{%\s{0,}block:(.*?)\s{0,}%}(.*?){%\s{0,}endblock\s{0,}%}/is', '<?php mako\view\Block::open(\'$1\'); ?>$2<?php mako\view\Block::close(); ?>', $template);
 
 		// Compile block output
 
-		return preg_replace('/{{\s{0,}block:(.*?)\s{0,}}}/', '<?php echo mako\view\Block::get(\'$1\'); ?>', $template);
+		return preg_replace('/{{\s{0,}block:(.*?)\s{0,}}}/i', '<?php echo mako\view\Block::get(\'$1\'); ?>', $template);
 	}
 
 	/**
