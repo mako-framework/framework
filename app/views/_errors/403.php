a44d0c302beb15cee0a90fc2dca1a9a5ddd3ab39
Frederic G. Østby <frederic.g.ostby@gmail.com>
2012-04-19 22:52:06 +0200

    New 404 and 403 views

--- app/views/_errors/403.php
+++ app/views/_errors/403.php
@@ -5,32 +5,55 @@
 <title>403 Forbidden</title>
 
 <style type="text/css">
+
 body
 {
         height:100%;
-        background:#8a8a8a;
+        background:#eee;
         padding:0px;
         margin:0px;
         height: 100%;
         font-size: 100%;
-        color:#fff;
-        font-family: Verdana, Arial, Helvetica, "Nimbus Sans", FreeSans, Malayalam, sans-serif;
-        text-shadow: 0px 1px 0px #555;
+        color:#333;
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
         line-height: 100%;
+}
+a
+{
+        color:#0088cc;
+        text-decoration:none;
+}
+a:hover
+{
+        color:#005580;
+        text-decoration:underline;
 }
 h1
 {
-        font-size: 300%;
+        font-size: 4em;
+}
+small
+{
+        font-size: 0.7em;
+        color: #999;
+        font-weight: normal;
 }
 hr
 {
         border:0px;
-        border-bottom:1px #b0b0b0 solid;
+        border-bottom:1px #ddd solid;
 }
 #message
 {
         width: 700px;
         margin: 15% auto;
+}
+#back-home
+{
+        bottom:0px;
+        right:0px;
+        position:absolute;
+        padding:10px;
 }
 </style>
 
@@ -38,10 +61,12 @@ hr
 <body>
 
 <div id="message">
-<h1>403</h1>
+<h1>404 <small>Forbidden</small></h1>
 <hr>
 <p>You don't have permission to access this page.</p>
 </div>
+
+<div id="back-home"><small>Would you like to <a href="<?php echo URL::base(); ?>">go back home</a>?</div>
 
 </body>
 </html>
