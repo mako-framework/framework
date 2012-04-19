ed2226dcf2bdd3dcbb00f71312934349bf11ffcb
Frederic G. Østby <frederic.g.ostby@gmail.com>
2012-04-19 22:57:12 +0200

    New error view

--- app/views/_errors/error.php
+++ app/views/_errors/error.php
@@ -1,36 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="<?=MAKO_CHARSET;?>">
+<meta charset="<?php echo MAKO_CHARSET; ?>">
 <title>Error</title>
 
 <style type="text/css">
+
 body
 {
         height:100%;
-        background:#8a8a8a;
+        background:#eee;
         padding:0px;
         margin:0px;
         height: 100%;
         font-size: 100%;
-        color:#fff;
-        font-family: Verdana, Arial, Helvetica, "Nimbus Sans", FreeSans, Malayalam, sans-serif;
-        text-shadow: 0px 1px 0px #555;
+        color:#333;
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
         line-height: 100%;
+}
+a
+{
+        color:#0088cc;
+        text-decoration:none;
+}
+a:hover
+{
+        color:#005580;
+        text-decoration:underline;
 }
 h1
 {
-        font-size: 300%;
+        font-size: 4em;
+}
+small
+{
+        font-size: 0.7em;
+        color: #999;
+        font-weight: normal;
 }
 hr
 {
         border:0px;
-        border-bottom:1px #b0b0b0 solid;
+        border-bottom:1px #ddd solid;
 }
 #message
 {
         width: 700px;
         margin: 15% auto;
+}
+#back-home
+{
+        bottom:0px;
+        right:0px;
+        position:absolute;
+        padding:10px;
 }
 </style>
 
@@ -42,6 +65,8 @@ hr
 <hr>
 <p>Aw, snap! An error has occurred while processing your request.</p>
 </div>
+
+<div id="back-home"><small>Would you like to <a href="<?php echo URL::base(); ?>">go back home</a>?</div>
 
 </body>
 </html>
