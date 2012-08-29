6f94debdc617366288ca0a975b97c4869bf8c765
Frederic G. Østby <frederic.g.ostby@gmail.com>
2012-08-29 10:00:00 +0200

    Removed functions that are a part of the Mako core

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,46 +4,6 @@
 // Define global convenience functions
 //------------------------------------------------------------------------------------------
 
-if(!function_exists('__'))
-{
-	/**
-	* Alias of mako\I18n::translate()
-	*
-	* Returns a translated string of the current language. 
-	* If no translation exists then the submitted string will be returned.
-	*
-	* @access  public
-	* @param   string   Text to translate
-	* @param   array   (optional) Value or array of values to replace in the translated text
-	* @param   string  (optional) Name of the language you want to translate to
-	* @return  string
-	*/
-
-	function __($string, array $vars = array(), $language = null)
-	{
-		return mako\I18n::translate($string, $vars, $language);
-	}
-}
-
-if(!function_exists('dump_var'))
-{
-	/**
-	* Works like var_dump except that it wraps the variable in <pre> tags.
-	*
-	* @access  public
-	* @param   mixed   Variable you want to dump
-	*/
-
-	function dump_var()
-	{
-		ob_start();
-
-		call_user_func_array('var_dump', func_get_args());
-
-		echo '<pre>' . ob_get_clean() . '</pre>';
-	}
-}
-
 if(!function_exists('e'))
 {
 	/**
