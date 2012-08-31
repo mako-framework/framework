c22f3ecffe693c1e833e1f9f881000f0809a21a9
Frederic G. Østby <frederic.g.ostby@gmail.com>
2012-08-31 18:14:36 +0200

    Fixed indentation

--- libraries/mako/Arr.php
+++ libraries/mako/Arr.php
@@ -144,7 +144,7 @@ class Arr
 
 	public static function isAssoc(array $array)
 	{
-	  return (bool) count(array_filter(array_keys($array), 'is_string'));
+		return (bool) count(array_filter(array_keys($array), 'is_string'));
 	}
 }
 
