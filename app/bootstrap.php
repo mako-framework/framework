01513c92e4a0c89df12673421e684413022cb56e
Frederic G. Østby <frederic.g.ostby@gmail.com>
2012-09-23 00:23:37 +0200

    Removed example code from bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,32 +4,12 @@
 // Define global convenience functions
 //------------------------------------------------------------------------------------------
 
-if(!function_exists('e'))
-{
-	/**
-	 * Returns a string where special characters have been converted to HTML entities.
-	 *
-	 * @access  public
-	 * @param   string   The string being converted.
-	 * @return  string
-	 */
-
-	function e($string)
-	{
-		return htmlspecialchars($string, ENT_QUOTES, MAKO_CHARSET);
-	}
-}
+// Nothing here
 
 //------------------------------------------------------------------------------------------
 // Setup autoloading of third party libraries
 //------------------------------------------------------------------------------------------
 
-// Zend Framework
-
-//mako\ClassLoader::directory(MAKO_LIBRARIES_PATH . '/Zend');
-
-// Swift Mailer
-
-//require MAKO_LIBRARIES_PATH . '/swiftmailer/swift_required.php'
+// Nothing here
 
 /** -------------------- End of file --------------------**/
