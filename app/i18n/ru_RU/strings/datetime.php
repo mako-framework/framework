d413bae14815738202b3b65737c377884cff7085
geekboy88 <geekboy88@gmail.com>
2013-01-11 11:05:08 +0400

    Russian language pack

---
+++ app/i18n/ru_RU/strings/datetime.php
@@ -0,0 +1,21 @@
+<?php
+
+//---------------------------------------------
+// Language file used by mako\DateTime
+//---------------------------------------------
+
+return array
+(
+	'datetime.today'       => 'сегодня',
+	'datetime.yesterday'   => 'вчера',
+	'datetime.minute_ago'  => 'минуту назад',
+	'datetime.minutes_ago' => '%1$u <pluralize:%1$u>минута</pluralize> назад',
+	'datetime.hour_ago'    => 'час назад',
+	'datetime.hours_ago'   => '%1$u <pluralize:%1$u>час</pluralize> назад',
+	'datetime.day_ago'     => 'день назад',
+	'datetime.days_ago'    => '%1$u <pluralize:%1$u>день</pluralize> назад',
+	'datetime.week_ago'    => 'неделю назад',
+	'datetime.weeks_ago'   => '%1$u <pluralize:%1$u>неделя</pluralize> назад',
+);
+
+/** -------------------- End of file --------------------**/
