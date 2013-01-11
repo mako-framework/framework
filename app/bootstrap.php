9344c957ede98cd0e2e91460c73041178d4a3e99
Frederic G. Østby <frederic.g.ostby@gmail.com>
2013-01-11 12:04:05 +0100

    Changed comment style

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@
 // Define global convenience functions and constants
 //------------------------------------------------------------------------------------------
 
-#define('MAKO_XHTML', 1); // Uncomment to return XHTML tags when using the HTML helper
+//define('MAKO_XHTML', 1); // Uncomment to return XHTML tags when using the HTML helper
 
 //------------------------------------------------------------------------------------------
 // Setup autoloading of third party libraries
