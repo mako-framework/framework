f79b4893f0fa8598d8141c17f619ff8ff826f197
Frederic G. Østby <frederic.g.ostby@gmail.com>
2013-01-18 21:10:39 +0100

    New and improved DateTime class

--- libraries/mako/DateTime.php
+++ libraries/mako/DateTime.php
@@ -3,68 +3,68 @@
 namespace mako;
 
 use \mako\I18n;
-use \OutOfRangeException;
+use \DateTimeZone;
 
 /**
-* Collection of date and time related methods.
+* Extension of the PHP DateTime class.
 *
 * @author     Frederic G. Østby
 * @copyright  (c) 2008-2013 Frederic G. Østby
 * @license    http://www.makoframework.com/license
 */
 
-class DateTime
+class DateTime extends \DateTime
 {
 	//---------------------------------------------
 	// Class properties
 	//---------------------------------------------
-	
+
 	/**
 	 * Number of seconds in a minute.
 	 *
 	 * @var int
 	 */
-	
+
 	const MINUTE = 60;
-	
+
 	/**
 	 * Number of seconds in an hour.
 	 *
 	 * @var int
 	 */
-	
+
 	const HOUR = 3600;
-	
+
 	/**
 	 * Number of seconds in a day.
 	 *
 	 * @var int
 	 */
-	
+
 	const DAY = 86400;
-	
+
 	/**
 	 * Number of seconds in a week.
 	 *
 	 * @var int
 	 */
-	
+
 	const WEEK = 604800;
-	
+
 	/**
-	 * Number of seconds in a month.
+	 * Average number of seconds in a month.
 	 *
 	 * @var int
 	 */
-	
+
 	const MONTH = 2629744;
-	
+
 	/**
-	 * Number of seconds in a year.
+	 * Average number of seconds in a year.
 	 *
 	 * @var int
 	 */
-	
+
 	const YEAR = 31556926;
 
 	//---------------------------------------------
@@ -72,14 +72,21 @@ class DateTime
 	//---------------------------------------------
 
 	/**
-	 * Protected constructor since this is a static class.
-	 *
-	 * @access  protected
+	 * Constructor.
+	 * 
+	 * @access  public
+	 * @param   string  $time      (optional) A date/time string
+	 * @param   mixed   $timeZone  (optional) A valid time zone or a DateTimeZone object
 	 */
 
-	protected function __construct()
+	public function __construct($time = 'now', $timeZone = null)
 	{
-		// Nothing here
+		if($timeZone !== null && !is_object($timeZone))
+		{
+			$timeZone = new DateTimeZone($timeZone);
+		}
+
+		parent::__construct($time, $timeZone);
 	}
 
 	//---------------------------------------------
@@ -87,90 +94,265 @@ class DateTime
 	//---------------------------------------------
 
 	/**
-	 * Returns a localized date (based on the locale).
-	 *
+	 * Returns a new DateTime object.
+	 * 
 	 * @access  public
-	 * @param   string  $dateFormat  Date format (http://php.net/manual/en/function.strftime.php)
-	 * @param   int     $timestamp   Unix timestamp
-	 * @param   int     $offset      (optional) Offset in seconds
-	 * @return  string
+	 * @param   mixed          $timeZone  (optional) A valid time zone or a DateTimeZone object
+	 * @return  mako\DateTime
 	 */
 
-	public static function utcDate($dateFormat, $timestamp, $offset = 0)
+	public static function now($timeZone = null)
 	{
-		return gmstrftime($dateFormat, ($timestamp + $offset));
+		return new static('now', $timeZone);
 	}
 
 	/**
-	 * Returns an array with all UTC timezones (keys are offset seconds from UTC).
-	 *
+	 * Returns new DateTime object according to the specified date.
+	 * 
+	 * @access  public
+	 * @param   int            $year      Year
+	 * @param   int            $month     (optional) Month (1 to 12)
+	 * @param   int            $day       (optional) Day of month (1 to 31)
+	 * @param   string         $timeZone  (optional) A valid time zone or a DateTimeZone object
+	 * @return  mako\DateTime
+	 */
+
+	public static function createFromDate($year, $month = null, $day = null, $timeZone = null)
+	{
+		$date = $year . '-' . ($month ?: date('m')) . '-' . ($day ?: date('d'));
+
+		return new static($date, $timeZone);
+	}
+
+	/**
+	 * Returns new DateTime object according to the specified timestamp.
+	 * 
+	 * @access  public
+	 * @param   int            $timestamp  Unix timestamp
+	 * @param   string         $timeZone   (optional) A valid time zone or a DateTimeZone object
+	 * @return  mako\DateTime
+	 */
+
+	public static function createFromTimestamp($timestamp, $timeZone = null)
+	{
+		$dateTime = new static('now', $timeZone);
+
+		$dateTime->setTimestamp($timestamp);
+
+		return $dateTime;
+	}
+
+	/**
+	 * Returns new DateTime object according to the specified DOS timestamp.
+	 * 
+	 * @access  public
+	 * @param   int            $timestamp  DOS timestamp
+	 * @param   string         $timeZone   (optional) A valid time zone or a DateTimeZone object
+	 * @return  mako\DateTime
+	 */
+
+	public static function createFromDosTimestamp($timestamp, $timeZone = null)
+	{
+		$year     = (($timestamp >> 25) & 0x7f) + 1980;
+		$mon      = ($timestamp >> 21) & 0x0f;
+		$mday     = ($timestamp >> 16) & 0x1f;
+		$hours    = ($timestamp >> 11) & 0x1f;
+		$minutes  = ($timestamp >> 5) & 0x3f;
+		$seconds  = 2 * ($timestamp & 0x1f);
+
+		$timestamp = mktime($hours, $minutes, $seconds, $mon, $mday, $year);
+
+		return static::createFromTimestamp($timestamp, $timeZone);
+	}
+
+	/**
+	 * Returns new DateTime object formatted according to the specified format.
+	 * 
+	 * @access  public
+	 * @param   string         $format    The format that the passed in string should be in
+	 * @param   string         $time      String representing the time
+	 * @param   string         $timeZone  (optional) A valid time zone or a DateTimeZone object
+	 * @return  mako\DateTime
+	 */
+
+	public static function createFromFormat($format, $time, $timeZone = null)
+	{
+		if($timeZone !== null)
+		{
+			if(!is_object($timeZone))
+			{
+				$timeZone = new DateTimeZone($timeZone);
+			}
+
+			$dateTime = parent::createFromFormat($format, $time, $timeZone);
+		}
+		else
+		{
+			$dateTime = parent::createFromFormat($format, $time);
+		}
+
+		return static::createFromTimestamp($dateTime->getTimestamp(), $dateTime->getTimeZone());
+	}
+
+	/**
+	 * Returns a list of time zones where the key is
+	 * a valid PHP time zone while the value is a presentable name.
+	 * 
 	 * @access  public
 	 * @return  array
 	 */
 
-	public static function utcTimeZones()
+	public static function getTimeZones()
 	{
-		$timeZones = array
-		(
-			'-43200' => 'UTC-12:00',
-			'-39600' => 'UTC-11:00',
-			'-36000' => 'UTC-10:00',
-			'-34200' => 'UTC-9:30',
-			'-32400' => 'UTC-9:00',
-			'-28800' => 'UTC-8:00',
-			'-25200' => 'UTC-7:00',
-			'-21600' => 'UTC-6:00',
-			'-18000' => 'UTC-5:00',
-			'-16200' => 'UTC-4:30',
-			'-14400' => 'UTC-4:00',
-			'-12600' => 'UTC-3:30',
-			'-10800' => 'UTC-3:00',
-			'-7200'  => 'UTC-2:00',
-			'-3600'  => 'UTC-1:00',
-			'0'      => 'UTC',
-			'3600'   => 'UTC+1:00',
-			'7200'   => 'UTC+2:00',
-			'10800'  => 'UTC+3:00',
-			'12600'  => 'UTC+3:30',
-			'14400'  => 'UTC+4:00',
-			'16200'  => 'UTC+4:30',
-			'18000'  => 'UTC+5:00',
-			'19800'  => 'UTC+5:30',
-			'20700'  => 'UTC+5:45',
-			'21600'  => 'UTC+6:00',
-			'23400'  => 'UTC+6:30',
-			'25200'  => 'UTC+7:00',
-			'28800'  => 'UTC+8:00',
-			'31500'  => 'UTC+8:45',
-			'32400'  => 'UTC+9:00',
-			'34200'  => 'UTC+9:30',
-			'36000'  => 'UTC+10:00',
-			'37800'  => 'UTC+10:30',
-			'39600'  => 'UTC+11:00',
-			'41400'  => 'UTC+11:30',
-			'43200'  => 'UTC+12:00',
-			'45900'  => 'UTC+12:45',
-			'46800'  => 'UTC+13:00',
-			'50400'  => 'UTC+14:00'
-		);
+		$timeZones = array();
+
+		foreach(DateTimeZone::listIdentifiers() as $timeZone)
+		{
+			$timeZones[$timeZone] = str_replace('_', ' ', $timeZone);
+		}
 
 		return $timeZones;
 	}
 
 	/**
-	 * Returns a "fuzzy date".
+	 * Returns an array of grouped time zones where the key is
+	 * a valid PHP timezone while the value is a presentable name.
+	 * 
+	 * @access  public
+	 * @return  array
+	 */
+
+	public static function getGroupedTimeZones()
+	{
+		$timeZones = array();
+
+		foreach(DateTimeZone::listIdentifiers() as $timeZone)
+		{
+			list($group, $city) = explode('/', $timeZone, 2) + array(null, null);
+
+			$timeZones[$group][$timeZone] = str_replace('_', ' ', $city);
+		}
+
+		unset($timeZones['UTC']);
+
+		return $timeZones;
+	}
+
+	/**
+	 * Move forward in time by x seconds.
+	 * 
+	 * @access  public
+	 * @param   int            $seconds  Number of seconds
+	 * @return  mako\DateTime
+	 */
+
+	public function moveForward($seconds)
+	{
+		$this->setTimestamp($this->getTimestamp() + $seconds);
+
+		return $this;
+	}
+
+	/**
+	 * Move backward in time by x seconds.
+	 * 
+	 * @access  public
+	 * @param   int            $seconds  Number of seconds
+	 * @return  mako\DateTime
+	 */
+
+	public function moveBackward($seconds)
+	{
+		$this->setTimestamp($this->getTimestamp() - $seconds);
+
+		return $this;
+	}
+
+	/**
+	 * Returns the DOS timestamp.
+	 * 
+	 * @access  public
+	 * @return  int
+	 */
+
+	public function getDosTimestamp()
+	{
+		$time = getdate($this->getTimestamp());
+
+		if($time['year'] < 1980)
+		{
+			$time['year']    = 1980;
+			$time['mon']     = 1;
+			$time['mday']    = 1;
+			$time['hours']   = 0;
+			$time['minutes'] = 0;
+			$time['seconds'] = 0;
+		}
+
+		return (($time['year'] - 1980) << 25) | ($time['mon'] << 21) | ($time['mday'] << 16) | ($time['hours'] << 11) | ($time['minutes'] << 5) | ($time['seconds'] >> 1);
+	}
+
+	/**
+	 * Returns TRUE if the year is a leap year and FALSE if not.
+	 * 
+	 * @access  public
+	 * @return  boolean
+	 */
+
+	public function isLeapYear()
+	{
+		$year = $this->format('Y');
+
+		if($year % 400 === 0 || ($year % 4 === 0 && $year % 100 !== 0))
+		{
+			return true;
+		}
+		 
+		return false;
+	}
+
+	/**
+	 * Returns the number of days in the current month.
+	 * 
+	 * @access  public
+	 * @return  int
+	 */
+
+	public function daysInMonth()
+	{
+		$days = array
+		(
+			31,
+			$this->isLeapYear() ? 29 : 28,
+			31,
+			30,
+			31,
+			30,
+			31,
+			31,
+			30,
+			31,
+			30,
+			31
+		);
+		 
+		return $days[$this->format('n') - 1];
+	}
+
+	/**
+	 * Returns a "fuzzy" date.
 	 *
 	 * @access  public
-	 * @param   int      $timestamp    Unix timestamp
-	 * @param   string   $dateFormat   Date format used for non-fuzy dates
-	 * @param   int      $offset       (optional) Offset in seconds
 	 * @param   boolean  $fullFuzzy    (optional) Enable full fuzziness
+	 * @param   string   $dateFormat   (optional) Date format used for non-fuzy dates
 	 * @param   string   $clockFormat  (optional) Clock format for fuzzy dates - set to false to disable
 	 * @return  string
 	 */
 
-	public static function fuzzy($timestamp, $dateFormat, $offset = 0, $fullFuzzy = false, $clockFormat = ', %R %p')
+	public function fuzzy($fullFuzzy = false, $dateFormat = 'Y-m-d', $clockFormat = ', H:i a')
 	{
+		$timestamp = $this->getTimestamp();
+
 		$now  = time();
 		$diff = max($now - $timestamp, 0);
 		
@@ -213,137 +395,35 @@ class DateTime
 			}
 			else
 			{
-				return static::utcDate($dateFormat, $timestamp, $offset);
+				$date = $this->format($dateFormat);
 			}
 		}
 		else
 		{
 			// Kinda fuzzy
 			
-			$compare = gmdate('jny', ($timestamp + $offset));
+			$compare = static::createFromTimestamp($timestamp)->format('jny');
 
-			if(gmdate('jny', ($now + $offset)) == $compare)
+			if(static::now()->format('jny') == $compare)
 			{
 				$date = I18n::translate('datetime.today');
 			}
-			elseif(gmdate('jny', (($now + $offset) - 86400)) == $compare)
+			elseif(static::now()->moveBackward(86400)->format('jny') == $compare)
 			{
 				$date = I18n::translate('datetime.yesterday');
 			}
 			else
 			{
-				return static::utcDate($dateFormat, $timestamp, $offset);
+				$date = $this->format($dateFormat);
 			}
 		}
 		
 		if($clockFormat !== false)
 		{
-			$date .= static::utcDate($clockFormat, $timestamp, $offset);
+			$date .= $this->format($clockFormat);
 		}
 		
 		return $date;
-	}
-
-	/**
-	 * Returns true if year is leap year and false if not.
-	 *
-	 * @access  public
-	 * @param   int      $year  The year you want to check
-	 * @return  boolean
-	 */
-
-	public static function isLeapYear($year)
-	{
-		if($year % 400 === 0 || ($year % 4 === 0 && $year % 100 !== 0))
-		{
-			return true;
-		}
-
-		return false;
-	}
-
-	/**
-	 * Returns the number of days in the chosen month.
-	 *
-	 * @access  public
-	 * @param   int     $month  (optional) Numeric representation of a month without leading zeros
-	 * @param   int     $year   (optional) Full numeric representation of a year
-	 * @return  int
-	 */
-
-	public static function daysInMonth($month = null, $year = null)
-	{
-		if($month !== null && ($month > 12 || $month < 1))
-		{
-			throw new OutOfRangeException(vsprintf("%s(): There are only 12 months in a year.", array(__METHOD__)));
-		}
-
-		$month === null && $month = date('n');
-
-		$year === null && $year = date('Y');
-
-		$days = array
-		(
-			31,
-			static::isLeapYear($year) ? 29 : 28,
-			31,
-			30,
-			31,
-			30,
-			31,
-			31,
-			30,
-			31,
-			30,
-			31
-		);
-
-		return $days[$month - 1];
-	}
-
-	/**
-	 * Converts a UNIX timestamp to a DOS timestamp.
-	 *
-	 * @access  public
-	 * @param   int     $timestamp  Unix timestamp
-	 * @return  int
-	 */
-
-	public static function unix2dos($timestamp = null)
-	{
-		$time = ($timestamp === null) ? getdate() : getdate($timestamp);
-
-		if($time['year'] < 1980)
-		{
-			$time['year']    = 1980;
-			$time['mon']     = 1;
-			$time['mday']    = 1;
-			$time['hours']   = 0;
-			$time['minutes'] = 0;
-			$time['seconds'] = 0;
-		}
-
-		return (($time['year'] - 1980) << 25) | ($time['mon'] << 21) | ($time['mday'] << 16) | ($time['hours'] << 11) | ($time['minutes'] << 5) | ($time['seconds'] >> 1);
-	}
-
-	/**
-	 * Converts a DOS timestamp to a UNIX timestamp.
-	 *
-	 * @access  public
-	 * @param   int     $timestamp  DOS timestamp
-	 * @return  int
-	 */
-
-	public static function dos2unix($timestamp)
-	{
-		$year     = (($timestamp >> 25) & 0x7f) + 1980;
-		$mon      = ($timestamp >> 21) & 0x0f;
-		$mday     = ($timestamp >> 16) & 0x1f;
-		$hours    = ($timestamp >> 11) & 0x1f;
-		$minutes  = ($timestamp >> 5) & 0x3f;
-		$seconds  = 2 * ($timestamp & 0x1f);
-
-		return mktime($hours, $minutes, $seconds, $mon, $mday, $year);
 	}
 }
 
