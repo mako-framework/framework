abfbc8ef00abd5d92bd6369db7594873b316d786
geekboy88 <geekboy88@gmail.com>
2013-01-19 13:35:25 +0400

    Added ability to define request's method
    Useful temporary solution for Rest controllers

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -3,11 +3,42 @@
 namespace app\controllers;
 
 use \mako\View;
+use \mako\Request;
 	
 class Index extends \mako\Controller
 {
+
+	const RESTFUL = true;
+
 	public function action_index()
 	{
-		return new View('welcome');
+		var_dump('action_index');
+		Request::factory('index/index', 'GET')->execute();
 	}
+
+	public function get_index()
+	{
+		var_dump('get_index');
+	}
+
+	public function post_index()
+	{
+		var_dump('post_index');
+	}
+
+	public function put_index()
+	{
+		var_dump('put_index');
+	}
+
+	public function delete_index()
+	{
+		var_dump('delete_index');
+		Request::factory('index/index', 'GET')->execute();
+	}
+
+	// public function delete_index()
+	// {
+
+	// }
 }
