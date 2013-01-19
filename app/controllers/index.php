2567871cabf8fdbf78f850df34fb1a74ad341278
geekboy88 <geekboy88@gmail.com>
2013-01-19 14:59:50 +0400

    Cleaning up

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -3,42 +3,11 @@
 namespace app\controllers;
 
 use \mako\View;
-use \mako\Request;
-	
+
 class Index extends \mako\Controller
 {
-
-	const RESTFUL = true;
-
 	public function action_index()
 	{
-		var_dump('action_index');
-		Request::factory('index/index', 'GET')->execute();
+		return new View('welcome');
 	}
-
-	public function get_index()
-	{
-		var_dump('get_index');
-	}
-
-	public function post_index()
-	{
-		var_dump('post_index');
-	}
-
-	public function put_index()
-	{
-		var_dump('put_index');
-	}
-
-	public function delete_index()
-	{
-		var_dump('delete_index');
-		Request::factory('index/index', 'GET')->execute();
-	}
-
-	// public function delete_index()
-	// {
-
-	// }
 }
