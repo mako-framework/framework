0426edcf95a7dd1c3625dc63d63685df5f748406
Frederic G. Østby <frederic.g.ostby@gmail.com>
2013-03-15 16:58:09 +0100

    Bumping version number

--- libraries/mako/_init.php
+++ libraries/mako/_init.php
@@ -4,7 +4,7 @@
 // Define some constants
 //------------------------------------------------------------------------------------------
 
-define('MAKO_VERSION', '3.2.1');
+define('MAKO_VERSION', '3.2.2');
 define('MAKO_START', microtime(true));
 define('MAKO_MAGIC_QUOTES', get_magic_quotes_gpc());
 define('MAKO_IS_WINDOWS', (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN'));
