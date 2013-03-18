4753814f870ff20f4b1c45f06550dc4676f5d6b9
Frederic G. Østby <frederic.g.ostby@gmail.com>
2013-03-18 14:18:31 +0100

    Renaming variable

--- libraries/mako/Request.php
+++ libraries/mako/Request.php
@@ -416,11 +416,11 @@ class Request
 		{
 			if($controller::RESTFUL === true)
 			{
-				$requestTypes = array('get', 'post', 'put', 'delete', 'patch');
+				$requestMethods = array('get', 'post', 'put', 'delete', 'patch');
 
-				foreach($requestTypes as $requestType)
+				foreach($requestMethods as $requestMethod)
 				{
-					if($controllerClass->hasMethod($requestType . '_' . $this->action))
+					if($controllerClass->hasMethod($requestMethod . '_' . $this->action))
 					{
 						throw new RequestException(405); // Only throw 405 if the controller has an action that can respond to the requested route
 					}
