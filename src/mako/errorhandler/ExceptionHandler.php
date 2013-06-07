9e8a9cfd34eb9b3ad1ea37226f19cf4fe852d3a3
Frederic G. Østby <frederic.g.ostby@gmail.com>
2013-06-07 21:20:59 +0200

    Now using Whoops to display exception info

--- src/mako/errorhandler/ExceptionHandler.php
+++ src/mako/errorhandler/ExceptionHandler.php
@@ -3,13 +3,15 @@
 namespace mako\errorhandler;
 
 use \Exception;
-use \ErrorException;
 use \mako\Log;
 use \mako\View;
 use \mako\Config;
-use \mako\Database;
+use \mako\Request;
 use \mako\Response;
 use \mako\reactor\CLI;
+use \Whoops\Run as Whoops;
+use \Whoops\Handler\PrettyPageHandler;
+use \Whoops\Handler\JsonResponseHandler;
 
 /**
  * Exception handler.
@@ -54,219 +56,81 @@ class ExceptionHandler
 	//---------------------------------------------
 
 	/**
-	 * Returns the code that triggered the error.
-	 * 
-	 * @access  protected
-	 * @param   string     $file     File where the error was triggered
-	 * @param   int        $line     Line where the error was triggered
-	 * @param   int        $padding  (optional) Lines of padding
-	 * @return  string
-	 */
-
-	protected function getCode($file, $line, $padding = 5)
-	{
-		if(!is_readable($file))
-		{
-			return;
-		}
-
-		$handle      = fopen($file, 'r');
-		$code        = '';
-		$currentLine = 0;
-
-		while(!feof($handle))
-		{
-			$currentLine++;
-
-			$temp = fgets($handle);
-
-			if($currentLine > $line + $padding)
-			{
-				break; // Exit loop after we have found what we were looking for
-			}
-
-			if($currentLine >= ($line - $padding) && $currentLine <= ($line + $padding))
-			{
-				$code .= (trim($temp) === '') ? " \n" : $temp; // Workaround for prettify bug
-			}
-		}
-
-		fclose($handle);
-
-		return array('start' => ($line - $padding), 'code' => $code);
-	}
-
-	/**
-	 * Modifies the backtrace array.
-	 *
-	 * @access  protected
-	 * @param   array      $backtrace  Array returned by the getTrace() method of an exception object
-	 * @return  array
-	 */
-
-	protected function formatBacktrace($backtrace)
-	{
-		if(is_array($backtrace) === false || count($backtrace) === 0)
-		{
-			return $backtrace;
-		}
-		
-		// Remove unnecessary info from backtrace
-		
-		if($backtrace[0]['function'] == '{closure}')
-		{
-			unset($backtrace[0]);
-		}
-		
-		// Format backtrace
-
-		$trace = array();
-
-		foreach($backtrace as $entry)
-		{
-			// Function
-			
-			$function = '';
-			
-			if(isset($entry['class']))
-			{
-				$function .= $entry['class'] . $entry['type'];
-			}
-			
-			$function .= $entry['function'] . '()';
-			
-			// Arguments
-			
-			$arguments = array();
-			
-			if(isset($entry['args']) && count($entry['args']) > 0)
-			{
-				foreach($entry['args'] as $arg)
-				{
-					ob_start();
-
-					var_dump($arg);
-
-					$arg = htmlspecialchars(ob_get_contents());
-
-					ob_end_clean();
-
-					$arguments[] = $arg;
-				}
-			}
-			
-			// Location
-			
-			$location = array();
-			
-			if(isset($entry['file']))
-			{
-				$location['file']   = $entry['file'];
-				$location['line']   = $entry['line'];
-				$location['source'] = $this->getCode($entry['file'], $entry['line']);
-			}
-			
-			// Compile into array
-			
-			$trace[] = array
-			(
-				'function'  => $function,
-				'arguments' => $arguments,
-				'location'  => $location,
-			);
-		}
-
-		return $trace;
-	}
-
-	/**
 	 * Displays the error in a CLI friendly format.
 	 * 
 	 * @access  protected
-	 * @param   array      $error  Error info
 	 */
 
-	protected function displayCLI($error)
+	protected function displayCLI()
 	{
 		$cli = new CLI();
 
-		$code = $this->getCode($error['file'], $error['line'], 0);
+		$message = vsprintf('%s: %s in %s at line %s' . PHP_EOL . PHP_EOL . '%s', array
+		(
+			get_class($this->exception),
+			$this->exception->getMessage(),
+			$this->exception->getFile(),
+			$this->exception->getLine(),
+			$this->exception->getTraceAsString(),
+		));
 
-		if(!empty($code))
-		{
-			$code = trim($code['code']);
-		}
-		else
-		{
-			$code = 'Unable to retrieve source code';
-		}
-
-		$output  = $error['type'] . PHP_EOL . PHP_EOL;
-		$output .= $error['message'] . ' in ' . $error['file'] . ' on line ' . $error['line'] . PHP_EOL . PHP_EOL;
-		$output .= str_repeat('-', $cli->screenWidth()) . PHP_EOL . PHP_EOL;
-		$output .=  '    ' . $code . PHP_EOL . PHP_EOL;
-		$output .= str_repeat('-', $cli->screenWidth()) . PHP_EOL . PHP_EOL;
-		$output .= $this->exception->getTraceAsString() . PHP_EOL;
-
-		$cli->stderr($output);
+		$cli->stderr($message . PHP_EOL);
 	}
 
 	/**
 	 * Displays the error in a web friendly format.
 	 * 
 	 * @access  protected
-	 * @param   array      $error  Error info
 	 */
 
-	protected function displayWeb($error)
+	protected function displayWeb()
 	{
+		$response = new Response();
+
 		if(Config::get('application.error_handler.display_errors') === true)
 		{
-			$error['source'] = $this->getCode($error['file'], $error['line']);
+			$whoops = new Whoops();
 
-			$error['queries'] = Database::getLog();
+			$whoops->allowQuit(false);
 
-			$backtrace = $this->exception->getTrace();
+			$whoops->writeToOutput(false);
 
-			if($this->exception instanceof ErrorException)
+			if(($request = Request::main()) instanceof Request && $request->isAjax())
 			{
-				$backtrace = array_slice($backtrace, 1); //Remove call to error handler from backtrace
+				$handler = new JsonResponseHandler();
+
+				$response->type('application/json');
+			}
+			else
+			{
+				$handler = new PrettyPageHandler();
+
+				switch($editor = Config::get('application.error_handler.open_with'))
+				{
+					case 'emacs':
+					case 'macvim':
+					case 'sublime':
+					case 'textmate':
+					case 'xdebug':
+						$handler->setEditor($editor);
+					break;
+					default:
+						$handler->setEditor(function($file, $line) use ($editor) {
+							return $editor;
+						});
+				}
 			}
 
-			$error['backtrace'] = $this->formatBacktrace($backtrace);
+			$whoops->pushHandler($handler);
 
-			Response::factory(new View('_mako_.errors.exception', array
-			(
-				'error'     => $error,
-				'open_with' => function($file, $line)
-				{
-					return sprintf(Config::get('application.error_handler.open_with'), $file, $line);
-				},
-			)))->send(500);
+			$response->body($whoops->handleException($this->exception));
 		}
 		else
 		{
-			Response::factory(new View('_mako_.errors.error'))->send(500);
+			$response->body(new View('_mako_.errors.error'));
 		}
-	}
 
-	/**
-	 * Displays the error.
-	 * 
-	 * @access  protected
-	 * @param   array      $error  Error info
-	 */
-
-	protected function display($error)
-	{
-		if(strtolower(PHP_SAPI) === 'cli')
-		{
-			$this->displayCLI($error);
-		}
-		else
-		{
-			$this->displayWeb($error);
-		}
+		$response->send(500);
 	}
 
 	/**
@@ -277,56 +141,31 @@ class ExceptionHandler
 
 	public function handle()
 	{
-		// Get exception info
+		// Display error
 
-		$error = array
-		(
-			'code'    => $this->exception->getCode(),
-			'message' => $this->exception->getMessage(),
-			'file'    => $this->exception->getFile(),
-			'line'    => $this->exception->getLine(),
-		);
-
-		// Determine error type
-
-		if($this->exception instanceof ErrorException)
+		if(strtolower(PHP_SAPI) === 'cli')
 		{
-			$error['type'] = 'ErrorException: ';
-
-			$codes = array
-			(
-				E_ERROR             => 'Fatal Error',
-				E_PARSE             => 'Parse Error',
-				E_COMPILE_ERROR     => 'Compile Error',
-				E_COMPILE_WARNING   => 'Compile Warning',
-				E_STRICT            => 'Strict Mode Error',
-				E_NOTICE            => 'Notice',
-				E_WARNING           => 'Warning',
-				E_RECOVERABLE_ERROR => 'Recoverable Error',
-				E_DEPRECATED        => 'Deprecated',
-				E_USER_NOTICE       => 'Notice',
-				E_USER_WARNING      => 'Warning',
-				E_USER_ERROR        => 'Error',
-				E_USER_DEPRECATED   => 'Deprecated'
-			);
-
-			$error['type'] .= in_array($error['code'], array_keys($codes)) ? $codes[$error['code']] : 'Unknown Error';
+			$this->displayCLI();
 		}
 		else
 		{
-			$error['type'] = get_class($this->exception);
+			$this->displayWeb();	
 		}
 
 		// Write to error log
 
 		if(Config::get('application.error_handler.log_errors') === true)
 		{
-			Log::error("{$error['type']}: {$error['message']} in {$error['file']} at line {$error['line']}");
+			$message = vsprintf('%s: %s in %s at line %s', array
+			(
+				get_class($this->exception),
+				$this->exception->getMessage(),
+				$this->exception->getFile(),
+				$this->exception->getLine(),
+			));
+
+			Log::error($message);
 		}
-
-		// Display error
-
-		$this->display($error);
 	}
 }
 
