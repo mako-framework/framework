aa3bce4ba0f41ce215d4ec413a072d008546fb61
Frederic G. Østby <frederic.g.ostby@gmail.com>
2013-09-18 12:39:07 +0200

    Added URL::toRoute method

--- src/mako/http/routing/URL.php
+++ src/mako/http/routing/URL.php
@@ -4,6 +4,8 @@ namespace mako\http\routing;
 
 use \mako\core\Config;
 use \mako\http\Request;
+use \mako\http\routing\Routes;
+use \RuntimeException;
 
 /**
  * URL helper.
@@ -98,72 +100,123 @@ class URL
 	}
 
 	/**
-	 * Returns a mako framework URL.
+	 * Returns the URL of the specified route.
 	 *
 	 * @access  public
-	 * @param   string   $route      URL segments
-	 * @param   array    $params     (optional) Associative array used to build URL-encoded query string
-	 * @param   string   $separator  (optional) Argument separator
-	 * @param   mixed    $language   (optional) Request language
+	 * @param   string   $route        URL segments
+	 * @param   array    $queryParams  (optional) Associative array used to build URL-encoded query string
+	 * @param   string   $separator    (optional) Argument separator
+	 * @param   mixed    $language     (optional) Request language
 	 * @return  string
 	 */
 
-	public static function to($route = '', array $params = array(), $separator = '&amp;', $language = true)
+	public static function to($route = '', array $queryParams = array(), $separator = '&amp;', $language = true)
 	{
 		$url = static::base() . (static::$clean ? '' : '/index.php') . ($language === true ? static::$language : (!$language ? '' : '/' . $language)) . $route;
 		
-		if(!empty($params))
+		if(!empty($queryParams))
 		{
-			$url .= '?' . http_build_query($params, '', $separator);
+			$url .= '?' . http_build_query($queryParams, '', $separator);
 		}
 		
 		return $url;
 	}
 
 	/**
-	 * Returns a mako framework URL.
-	 *
+	 * Returns the URL of a named route.
+	 * 
 	 * @access  public
-	 * @param   string   $route      URL segments
-	 * @param   mixed    $language   (optional) Request language
-	 * @param   array    $params     (optional) Associative array used to build URL-encoded query string
-	 * @param   string   $separator  (optional) Argument separator
+	 * @param   string  $routeName  Route name
+	 * @param   array   $routeParams  (optional) Route parameters
+	 * @param   array   $queryParams  (optional) Associative array used to build URL-encoded query string
+	 * @param   string  $separator    (optional) Argument separator
+	 * @param   mixed   $language     (optional) Request language
 	 * @return  string
 	 */
 
-	public static function toLanguage($route = '', $language = true, array $params = array(), $separator = '&amp;')
+	public static function toRoute($routeName, array $routeParams = array(), $queryParams = array(), $separator = '&amp;', $language = true)
 	{
-		return static::to($route, $params, $separator, $language);
+		if(!($route = Routes::getNamedRoute($routeName)))
+		{
+			throw new RuntimeException(vsprintf("%s(): No route named '%s' has been defined.", array(__METHOD__, $routeName)));
+		}
+
+		$route = $route->getRoute();
+
+		foreach($routeParams as $key => $value)
+		{
+			$route = preg_replace('/{' . $key . '}\??/', $value, $route, 1);
+		}
+
+		if(strpos($route, '?') !== false)
+		{
+			$route = preg_replace('/{\w+}\?/', '', $route);
+		}
+
+		return URL::to($route, $queryParams, $separator, $language);
+	}
+
+	/**
+	 * Returns the URL of the specified route.
+	 *
+	 * @access  public
+	 * @param   string   $route        URL segments
+	 * @param   mixed    $language     (optional) Request language
+	 * @param   array    $queryParams  (optional) Associative array used to build URL-encoded query string
+	 * @param   string   $separator    (optional) Argument separator
+	 * @return  string
+	 */
+
+	public static function toLanguage($route = '', $language = true, array $queryParams = array(), $separator = '&amp;')
+	{
+		return static::to($route, $queryParams, $separator, $language);
+	}
+
+	/**
+	 * Returns the URL of a named route.
+	 * 
+	 * @access  public
+	 * @param   string  $routeName  Route name
+	 * @param   array   $routeParams  (optional) Route parameters
+	 * @param   mixed   $language     (optional) Request language
+	 * @param   array   $queryParams  (optional) Associative array used to build URL-encoded query string
+	 * @param   string  $separator    (optional) Argument separator
+	 * @return  string
+	 */
+
+	public static function toRouteLanguage($routeName, array $routeParams = array(), $language = true, array $queryParams = array(), $separator = '&amp;')
+	{
+		return static::toRoute($routeName, $routeParams, $queryParams, $separator, $language);
 	}
 
 	/**
 	 * Returns the current URL of the main request.
 	 *
 	 * @access  public
-	 * @param   array    $params     (optional) Associative array used to build URL-encoded query string
-	 * @param   string   $separator  (optional) Argument separator
-	 * @param   mixed    $language   (optional) Request language
+	 * @param   array    $queryParams  (optional) Associative array used to build URL-encoded query string
+	 * @param   string   $separator    (optional) Argument separator
+	 * @param   mixed    $language     (optional) Request language
 	 * @return  string
 	 */
 
-	public static function current(array $params = array(), $separator = '&amp;', $language = true)
+	public static function current(array $queryParams = array(), $separator = '&amp;', $language = true)
 	{
-		return static::to(Request::main()->route(), $params, $separator, $language);
+		return static::to(Request::main()->route(), $queryParams, $separator, $language);
 	}
 
 	/**
 	 * Returns the current URL of the main request.
 	 *
 	 * @access  public
-	 * @param   mixed    $language   (optional) Request language
-	 * @param   array    $params     (optional) Associative array used to build URL-encoded query string
-	 * @param   string   $separator  (optional) Argument separator
+	 * @param   mixed    $language     (optional) Request language
+	 * @param   array    $queryParams  (optional) Associative array used to build URL-encoded query string
+	 * @param   string   $separator    (optional) Argument separator
 	 * @return  string
 	 */
 
-	public static function currentLanguage($language = true, array $params = array(), $separator = '&amp;')
+	public static function currentLanguage($language = true, array $queryParams = array(), $separator = '&amp;')
 	{
-		return static::current($params, $separator, $language);
+		return static::current($queryParams, $separator, $language);
 	}
 
 	/**
