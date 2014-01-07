06cb699bec0c0d5d67b223bd7fb3c8e68275cd28
Frederic G. Østby <frederic.g.ostby@gmail.com>
2014-01-07 16:53:20 +0100

    Fixed code style and added some extra comments

--- src/mako/reactor/tasks/server/router.php
+++ src/mako/reactor/tasks/server/router.php
@@ -3,15 +3,21 @@
 $requestURI   = $_SERVER['REQUEST_URI'];
 $documentRoot = $_SERVER['DOCUMENT_ROOT'];
 
-// Strip the query strng
-// Need to strip one extra spot for the '?'
-if (isset($_SERVER['QUERY_STRING']))
-	$requestURI = substr($requestURI, 0, strrpos($requestURI, $_SERVER['QUERY_STRING']) - 1);
+// Strip the query stringso that files can be served even if they have a query string
+// This can be useful for versioning assets such as images, CSS and JavaScript
 
+if (isset($_SERVER['QUERY_STRING']))
+{
+	$requestURI = substr($requestURI, 0, strrpos($requestURI, $_SERVER['QUERY_STRING']) - 1);
+}
+
+// Check if the file exists
 
 if($requestURI !== '/' && file_exists($documentRoot . '/' . $requestURI))
 {
 	return false; // serve the requested resource as-is.
 }
+
+// File doesn't exist. Route request to index.php
 
 require $documentRoot . '/index.php';
