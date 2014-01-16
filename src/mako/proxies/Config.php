817c4ea746659d737d229cc2ff6cc26cfdcb7e49
Frederic G. Østby <frederic.g.ostby@gmail.com>
2014-01-16 16:13:23 +0100

    Added config proxy

---
+++ src/mako/proxies/Config.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace mako\proxies;
+
+use \mako\core\Application;
+
+/**
+ * Config proxy.
+ *
+ * @author     Frederic G. Østby
+ * @copyright  (c) 2008-2013 Frederic G. Østby
+ * @license    http://www.makoframework.com/license
+ */
+
+class Config extends \mako\proxies\Proxy
+{
+	/**
+	 * Returns instance of the class we're proxying.
+	 * 
+	 * @access  protected
+	 * @return  \mako\core\Config
+	 */
+
+	protected static function instance()
+	{
+		return Application::instance(null)->get('config');
+	}
+}
+
+/** -------------------- End of file -------------------- **/
