efc561977894dbb6f4247d690dc5bb8b4daa360b
Frederic G. Østby <frederic.g.ostby@gmail.com>
2014-01-29 13:21:06 +0100

    Cleaning up

--- src/mako/utility/File.php
+++ src/mako/utility/File.php
@@ -2,13 +2,8 @@
 
 namespace mako\utility;
 
-use \Closure;
-use \mako\core\Config;
-use \mako\security\crypto\Crypto;
-use \mako\security\crypto\adapters\Adapter;
-
 /**
- * Collection of file related methods.
+ * Collection of file utlity methods.
  *
  * @author     Frederic G. Østby
  * @copyright  (c) 2008-2013 Frederic G. Østby
@@ -21,7 +16,83 @@ class File
 	// Class properties
 	//---------------------------------------------
 
-	// Nothing here
+	/**
+	 * Mime types.
+	 * 
+	 * @var array
+	 */
+
+	protected static $mimes =
+	[
+		'aac'        => 'audio/aac',
+		'atom'       => 'application/atom+xml',
+		'avi'        => 'video/avi',
+		'bmp'        => 'image/x-ms-bmp',
+		'c'          => 'text/x-c',
+		'class'      => 'application/octet-stream',
+		'css'        => 'text/css',
+		'csv'        => 'text/csv',
+		'deb'        => 'application/x-deb',
+		'dll'        => 'application/x-msdownload',
+		'dmg'        => 'application/x-apple-diskimage',
+		'doc'        => 'application/msword',
+		'docx'       => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+		'exe'        => 'application/octet-stream',
+		'flv'        => 'video/x-flv',
+		'gif'        => 'image/gif',
+		'gz'         => 'application/x-gzip',
+		'h'          => 'text/x-c',
+		'htm'        => 'text/html',
+		'html'       => 'text/html',
+		'ics'        => 'text/calendar',
+		'ical'       => 'text/calendar',
+		'ini'        => 'text/plain',
+		'jar'        => 'application/java-archive',
+		'java'       => 'text/x-java',
+		'jpeg'       => 'image/jpeg',
+		'jpg'        => 'image/jpeg',
+		'js'         => 'text/javascript',
+		'json'       => 'application/json',
+		'jp2'        => 'image/jp2',
+		'mid'        => 'audio/midi',
+		'midi'       => 'audio/midi',
+		'mka'        => 'audio/x-matroska',
+		'mkv'        => 'video/x-matroska',
+		'mp3'        => 'audio/mpeg',
+		'mp4'        => 'video/mp4',
+		'mpeg'       => 'video/mpeg',
+		'mpg'        => 'video/mpeg',
+		'm4a'        => 'video/mp4',
+		'm4v'        => 'video/mp4',
+		'odt'        => 'application/vnd.oasis.opendocument.text',
+		'ogg'        => 'audio/ogg',
+		'pdf'        => 'application/pdf',
+		'php'        => 'text/x-php',
+		'png'        => 'image/png',
+		'psd'        => 'image/vnd.adobe.photoshop',
+		'py'         => 'application/x-python',
+		'ra'         => 'audio/vnd.rn-realaudio',
+		'ram'        => 'audio/vnd.rn-realaudio',
+		'rar'        => 'application/x-rar-compressed',
+		'rss'        => 'application/rss+xml',
+		'safariextz' => 'application/x-safari-extension',
+		'sh'         => 'text/x-shellscript',
+		'shtml'      => 'text/html',
+		'swf'        => 'application/x-shockwave-flash',
+		'tar'        => 'application/x-tar',
+		'tif'        => 'image/tiff',
+		'tiff'       => 'image/tiff',
+		'torrent'    => 'application/x-bittorrent',
+		'txt'        => 'text/plain',
+		'wav'        => 'audio/wav',
+		'webp'       => 'image/webp',
+		'wma'        => 'audio/x-ms-wma',
+		'xls'        => 'application/vnd.ms-excel',
+		'xml'        => 'text/xml',
+		'zip'        => 'application/zip',
+		'3gp'        => 'video/3gpp',
+		'3g2'        => 'video/3gpp2',
+	];
 
 	//---------------------------------------------
 	// Class constructor, destructor etc ...
@@ -125,7 +196,7 @@ class File
 
 				$extension = pathinfo($file, PATHINFO_EXTENSION);
 
-				return Config::get('mimes.' . $extension, false);
+				return isset(static::$mimes[$extension]) ? static::$mimes[$extension] : false;
 			}
 			else
 			{
@@ -204,88 +275,6 @@ class File
 	public static function truncate($file, $lock = false)
 	{
 		return (0 === file_put_contents($file, null, $lock ? LOCK_EX : 0));
-	}
-
-	/**
-	 * Encrypts a file.
-	 * 
-	 * @access  public
-	 * @param   string                                   $file    File path
-	 * @param   \mako\security\crypto\adapters\Adapter   $crypto  (optional) Crypto adapter instance
-	 * @return  boolean
-	 */
-
-	public static function encrypt($file, Adapter $crypto = null)
-	{
-		$crypto = $crypto ?: Crypto::factory();
-
-		return (file_put_contents($file, $crypto->encrypt(file_get_contents($file))) !== false);
-	}
-
-	/**
-	 * Decrypts a file.
-	 * 
-	 * @access  public
-	 * @param   string                                   $file    File path
-	 * @param   \mako\security\crypto\adapters\Adapter   $crypto  (optional) Crypto adapter instance
-	 * @return  boolean
-	 */
-
-	public static function decrypt($file, Adapter $crypto = null)
-	{
-		$crypto = $crypto ?: Crypto::factory();
-
-		$decrypted = $crypto->decrypt(file_get_contents($file));
-
-		if($decrypted !== false)
-		{
-			return (false !== file_put_contents($file, $decrypted));
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	/**
-	 * Encrypts and signs a file.
-	 * 
-	 * @access  public
-	 * @param   string                                   $file    File path
-	 * @param   \mako\security\crypto\adapters\Adapter   $crypto  (optional) Crypto adapter instance
-	 * @return  boolean
-	 */
-
-	public static function encryptAndSign($file, Adapter $crypto = null)
-	{
-		$crypto = $crypto ?: Crypto::factory();
-
-		return (file_put_contents($file, $crypto->encryptAndSign(file_get_contents($file))) !== false);
-	}
-
-	/**
-	 * Validates and decrypts a file.
-	 * 
-	 * @access  public
-	 * @param   string                                   $file    File path
-	 * @param   \mako\security\crypto\adapters\Adapter   $crypto  (optional) Crypto adapter instance
-	 * @return  boolean
-	 */
-
-	public static function validateAndDecrypt($file, Adapter $crypto = null)
-	{
-		$crypto = $crypto ?: Crypto::factory();
-
-		$decrypted = $crypto->validateAndDecrypt(file_get_contents($file));
-
-		if($decrypted !== false)
-		{
-			return (false !== file_put_contents($file, $decrypted));
-		}
-		else
-		{
-			return false;
-		}
 	}
 }
 
