8198c58c1cdab39ad4d20a1f03e4c40c1af1c5ce
Frederic G. Østby <frederic.g.ostby@gmail.com>
2014-04-30 16:32:49 +0200

    More ORM tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,1 +1,5 @@
 <?php
+
+define('MAKO_TESTS_PATH', __DIR__);
+
+date_default_timezone_set('UTC');
--- tests/integration/database/midgard/ORMTest.php
+++ tests/integration/database/midgard/ORMTest.php
@@ -55,7 +55,9 @@ class ORMTest extends PHPUnit_Framework_TestCase
 
 		// Load test database into memory
 
-		$this->connectionManager->connection()->getPDO()->exec(file_get_contents(realpath(__DIR__ . '/../../resources/sqlite.sql')));
+		$sql = file_get_contents(MAKO_TESTS_PATH . '/integration/resources/sqlite.sql');
+
+		$this->connectionManager->connection()->getPDO()->exec($sql);
 
 		// Set the connection manager
 
@@ -81,10 +83,114 @@ class ORMTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals('foo@example.org', $user->email);
 	}
 
+	/**
+	 * 
+	 */
+
 	public function testGetNonExistent()
 	{
 		$user = TestUser::get(999);
 
 		$this->assertFalse($user);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testReload()
+	{
+		$user = TestUser::get(1);
+
+		$user->username = 'bax';
+
+		$this->assertEquals('bax', $user->username);
+
+		$reloaded = $user->reload();
+
+		$this->assertTrue($reloaded);
+
+		$this->assertEquals('foo', $user->username);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testReloadNonExistent()
+	{
+		$user = new TestUser;
+
+		$reloaded = $user->reload();
+
+		$this->assertFalse($reloaded);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testCreate()
+	{
+		$dateTime = new DateTime;
+
+		$user = TestUser::create(['username' => 'bax', 'email' => 'bax@example.org', 'created_at' => $dateTime]);
+
+		$this->assertEquals(4, $user->id);
+
+		$this->assertEquals('bax', $user->username);
+
+		$this->assertEquals('bax@example.org', $user->email);
+
+		$this->assertEquals($dateTime, $user->created_at);
+
+		$user->delete();
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testClone()
+	{
+		$user = TestUser::get(1);
+
+		$clone = clone $user;
+
+		$this->assertTrue(empty($clone->id));
+
+		$this->assertEquals($clone->created_at, $user->created_at);
+
+		$this->assertEquals($clone->username, $user->username);
+
+		$this->assertEquals($clone->email, $user->email);
+
+		$clone->save();
+
+		$this->assertEquals(4, $clone->id);
+
+		$clone->delete();
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testToArray()
+	{
+		$user = TestUser::get(1)->toArray();
+
+		$this->assertEquals(['id' => '1', 'created_at' => '2014-04-30 14:40:01', 'username' => 'foo', 'email' => 'foo@example.org'], $user);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testToJSON()
+	{
+		$user = TestUser::get(1)->toJSON();
+
+		$this->assertEquals('{"id":"1","created_at":"2014-04-30 14:40:01","username":"foo","email":"foo@example.org"}', $user);
 	}
 }
