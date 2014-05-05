e4ec23e78e8ca4fbbdf87352f699fd3929dcb3c7
Frederic G. Østby <frederic.g.ostby@gmail.com>
2014-05-05 12:08:17 +0200

    More ORM unit tests

--- tests/unit/database/midgard/ORMTest.php
+++ tests/unit/database/midgard/ORMTest.php
@@ -6,6 +6,14 @@ namespace mako\tests\unit\database\midgard;
 // START CLASSES
 // --------------------------------------------------------------------------
 
+class FakeRelation
+{
+	public function getRelated()
+	{
+		return 'fake relation';
+	}
+}
+
 class TestUser1 extends \mako\database\midgard\ORM
 {
 	protected $including = ['profile'];
@@ -20,6 +28,38 @@ class TestUser2 extends TestUser1
 	protected $enableLocking = true;
 
 	protected $readOnly = true;
+
+	protected function set_array($array)
+	{
+		return json_encode($array);
+	}
+
+	public function get_array($json)
+	{
+		return json_decode($json, true);
+	}
+
+	public function fake_relation()
+	{
+		return new FakeRelation();
+	}
+}
+
+class TestUser3 extends TestUser1
+{
+	protected $assignable = ['username', 'email'];
+}
+
+class TestUser4 extends TestUser1
+{
+	protected $protected = ['password'];
+
+	protected $columns = ['username' => 'foo', 'password' => 'bar', 'array' => '[1,2,3]'];
+
+	public function get_array($json)
+	{
+		return json_decode($json, true);
+	}
 }
 
 // --------------------------------------------------------------------------
@@ -162,5 +202,160 @@ class ORMTest extends \PHPUnit_Framework_TestCase
 		$user = new TestUser1($columns);
 
 		$this->assertEquals($columns, $user->getColumns());
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testSetandGetColumn()
+	{
+		$user = new TestUser1();
+
+		$user->array = [1, 2, 3];
+
+		$this->assertEquals([1, 2, 3], $user->array);
+
+		$this->assertEquals([1, 2, 3], $user->getColumn('array'));
+
+		$this->assertEquals([1, 2, 3], $user->getColumn('array', true));
+
+		//
+
+		$user = new TestUser1();
+
+		$user->setColumn('array', [1, 2, 3]);
+
+		$this->assertEquals([1, 2, 3], $user->array);
+
+		$this->assertEquals([1, 2, 3], $user->getColumn('array'));
+
+		$this->assertEquals([1, 2, 3], $user->getColumn('array', true));
+
+		//
+
+		$user = new TestUser2();
+
+		$user->array = [1, 2, 3];
+
+		$this->assertEquals([1, 2, 3], $user->array);
+
+		$this->assertEquals([1, 2, 3], $user->getColumn('array'));
+
+		$this->assertEquals('[1,2,3]', $user->getColumn('array', true));
+
+		//
+
+		$user = new TestUser2();
+
+		$user->setColumn('array', [1, 2, 3]);
+
+		$this->assertEquals([1, 2, 3], $user->array);
+
+		$this->assertEquals([1, 2, 3], $user->getColumn('array'));
+
+		$this->assertEquals('[1,2,3]', $user->getColumn('array', true));
+
+		//
+
+		$user = new TestUser2();
+
+		$user->setColumn('array', [1, 2, 3], true);
+
+		$this->assertEquals([1, 2, 3], $user->getColumn('array', true));
+
+		//
+
+		$user = new TestUser2();
+
+		$this->assertEquals('fake relation', $user->fake_relation);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testAssign()
+	{
+		$user = new TestUser3();
+
+		$user->assign(['username' => 'foo', 'email' => 'bar', 'is_admin' => 1]);
+
+		$this->assertEquals(['username' => 'foo', 'email' => 'bar'], $user->getColumns());
+
+		//
+
+		$user = new TestUser3(['username' => 'foo', 'email' => 'bar', 'is_admin' => 1]);
+
+		$this->assertEquals(['username' => 'foo', 'email' => 'bar'], $user->getColumns());
+
+		//
+
+		$user = new TestUser3();
+
+		$user->assign(['username' => 'foo', 'email' => 'bar', 'is_admin' => 1, 'id' => 1], false, false);
+
+		$this->assertEquals(['username' => 'foo', 'email' => 'bar', 'is_admin' => 1, 'id' => 1], $user->getColumns());
+
+		//
+
+		$user = new TestUser3(['username' => 'foo', 'email' => 'bar', 'is_admin' => 1, 'id' => 1], false, false);
+
+		$this->assertEquals(['username' => 'foo', 'email' => 'bar', 'is_admin' => 1, 'id' => 1], $user->getColumns());
+
+		//
+
+		$user = new TestUser3([], false, true, true);
+
+		$user->assign(['username' => 'foo', 'email' => 'bar', 'is_admin' => 1, 'id' => 1], false, false);
+
+		$this->assertEquals(['username' => 'foo', 'email' => 'bar', 'is_admin' => 1], $user->getColumns());
+
+		//
+
+		$user = new TestUser3(['username' => 'foo', 'email' => 'bar', 'is_admin' => 1, 'id' => 1], false, false, true);
+
+		$this->assertEquals(['username' => 'foo', 'email' => 'bar', 'is_admin' => 1, 'id' => 1], $user->getColumns());
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testToArray()
+	{
+		$user = new TestUser4();
+
+		$this->assertEquals(['username' => 'foo', 'array' => [1, 2, 3]], $user->toArray());
+
+		$this->assertEquals(['username' => 'foo', 'password' => 'bar', 'array' => [1, 2, 3]], $user->toArray(false));
+
+		$this->assertEquals(['username' => 'foo', 'password' => 'bar', 'array' => '[1,2,3]'], $user->toArray(false, true));
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testToJson()
+	{
+		$user = new TestUser4();
+
+		$this->assertEquals('{"username":"foo","array":[1,2,3]}', $user->toJson());
+
+		$this->assertEquals('{"username":"foo","password":"bar","array":[1,2,3]}', $user->toJson(false));
+
+		$this->assertEquals('{"username":"foo","password":"bar","array":"[1,2,3]"}', $user->toJson(false, true));
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testToString()
+	{
+		$user = new TestUser4();
+
+		$this->assertEquals('{"username":"foo","array":[1,2,3]}', (string) $user);
 	}
 }
