721adbc30f2970c9206b76d6cdac3b2c080e5d00
Frederic G. Østby <frederic.g.ostby@gmail.com>
2014-05-05 14:11:23 +0200

    ResultSet tests

---
+++ tests/unit/database/midgard/ResultSetTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace mako\tests\unit\database\midgard;
+
+use \mako\database\midgard\ResultSet;
+
+use \Mockery as m;
+
+class ResultSetTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * 
+	 */
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testPluck()
+	{
+		$resultSet = new ResultSet([['foo' => 'bar'], ['foo' => 'bar']]);
+
+		$plucked = $resultSet->pluck('foo');
+
+		$this->assertEquals(['bar', 'bar'], $plucked);
+
+		//
+
+		$resultSet = new ResultSet([(object) ['foo' => 'bar'], (object) ['foo' => 'bar']]);
+
+		$plucked = $resultSet->pluck('foo');
+
+		$this->assertEquals(['bar', 'bar'], $plucked);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testToArray()
+	{
+		$mock1 = m::mock('\mako\database\midgard\ORM')->shouldReceive('toArray')->once()->andReturn(['foo' => 'bar'])->getMock();
+
+		$mock2 = m::mock('\mako\database\midgard\ORM')->shouldReceive('toArray')->once()->andReturn(['foo' => 'baz'])->getMock();
+
+		$resultSet = new ResultSet([$mock1, $mock2]);
+
+		$this->assertEquals([['foo' => 'bar'], ['foo' => 'baz']], $resultSet->toArray());
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testToJson()
+	{
+		$mock1 = m::mock('\mako\database\midgard\ORM')->shouldReceive('toArray')->once()->andReturn(['foo' => 'bar'])->getMock();
+
+		$mock2 = m::mock('\mako\database\midgard\ORM')->shouldReceive('toArray')->once()->andReturn(['foo' => 'baz'])->getMock();
+
+		$resultSet = new ResultSet([$mock1, $mock2]);
+
+		$this->assertEquals('[{"foo":"bar"},{"foo":"baz"}]', $resultSet->toJson());
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testToString()
+	{
+		$mock1 = m::mock('\mako\database\midgard\ORM')->shouldReceive('toArray')->once()->andReturn(['foo' => 'bar'])->getMock();
+
+		$mock2 = m::mock('\mako\database\midgard\ORM')->shouldReceive('toArray')->once()->andReturn(['foo' => 'baz'])->getMock();
+
+		$resultSet = new ResultSet([$mock1, $mock2]);
+
+		$this->assertEquals('[{"foo":"bar"},{"foo":"baz"}]', (string) $resultSet);
+	}
+}
