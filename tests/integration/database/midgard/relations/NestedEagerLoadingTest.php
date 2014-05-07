e62e6cdc5d51f69af933a722abb66033252495a4
Frederic G. Østby <frederic.g.ostby@gmail.com>
2014-05-07 12:16:32 +0200

    Added tests for nested eager loading

---
+++ tests/integration/database/midgard/relations/NestedEagerLoadingTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace mako\tests\integration\database\midgard\relations;
+
+// --------------------------------------------------------------------------
+// START CLASSES
+// --------------------------------------------------------------------------
+
+class NestedEagerLoadingUser extends \TestORM
+{
+	protected $tableName = 'users';
+
+	public function articles()
+	{
+		return $this->hasMany('mako\tests\integration\database\midgard\relations\NestedEagerLoadingArticle', 'user_id');
+	}
+}
+
+class NestedEagerLoadingArticle extends \TestORM
+{
+	protected $tableName = 'articles';
+
+	public function comments()
+	{
+		return $this->hasMany('mako\tests\integration\database\midgard\relations\NestedEagerLoadingComment', 'article_id');
+	}
+}
+
+class NestedEagerLoadingComment extends \TestORM
+{
+	protected $tableName = 'article_comments';
+}
+
+// --------------------------------------------------------------------------
+// END CLASSES
+// --------------------------------------------------------------------------
+
+/**
+ * @group integration
+ * @group integration:database
+ * @requires extension PDO
+ * @requires extension pdo_sqlite
+ */
+
+class NestedEagerLoadingTest extends \ORMTestCase
+{
+	public function testNestedEagerLoading()
+	{
+		$queryCountBefore = count($this->connectionManager->connection('sqlite')->getLog());
+
+		$users = NestedEagerLoadingUser::including(['articles', 'articles.comments'])->ascending('id')->all();
+
+		foreach($users as $user)
+		{
+			$this->assertInstanceOf('mako\database\midgard\ResultSet', $user->articles);
+
+			foreach($user->articles as $article)
+			{
+				$this->assertInstanceOf('mako\tests\integration\database\midgard\relations\NestedEagerLoadingArticle', $article);
+
+				$this->assertEquals($article->user_id, $user->id);
+
+				$this->assertInstanceOf('mako\database\midgard\ResultSet', $article->comments);
+
+				foreach($article->comments as $comment)
+				{
+					$this->assertInstanceOf('mako\tests\integration\database\midgard\relations\NestedEagerLoadingComment', $comment);
+
+					$this->assertEquals($comment->article_id, $article->id);
+				}
+			}
+		}
+
+		$queryCountAfter = count($this->connectionManager->connection('sqlite')->getLog());
+
+		$this->assertEquals(3, $queryCountAfter - $queryCountBefore);
+	}
+}
--- tests/integration/resources/sqlite.sql
+++ tests/integration/resources/sqlite.sql
@@ -1,46 +1,6 @@
-DROP TABLE IF EXISTS "articles";
-CREATE TABLE "articles" (
-  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-  "user_id" integer NOT NULL,
-  "created_at" text NOT NULL,
-  "title" text NOT NULL,
-  "body" text NOT NULL
-);
-
-INSERT INTO "articles" ("id", "user_id", "created_at", "title", "body") VALUES (1, 1, '2014-04-30 15:02:10', 'article 1', 'article body 1');
-INSERT INTO "articles" ("id", "user_id", "created_at", "title", "body") VALUES (2, 1, '2014-04-30 15:02:10', 'article 2', 'article body 2');
-INSERT INTO "articles" ("id", "user_id", "created_at", "title", "body") VALUES (3, 2, '2014-04-30 15:32:10', 'article 3', 'article body 3');
-
-DROP TABLE IF EXISTS "groups";
-CREATE TABLE "groups" (
-  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-  "name" text NOT NULL
-);
-
-INSERT INTO "groups" ("id", "name") VALUES (1, 'admin');
-INSERT INTO "groups" ("id", "name") VALUES (2, 'user');
-
-DROP TABLE IF EXISTS "groups_users";
-CREATE TABLE "groups_users" (
-  "group_id" integer NOT NULL,
-  "user_id" integer NOT NULL
-);
-
-INSERT INTO "groups_users" ("group_id", "user_id") VALUES (1, 1);
-INSERT INTO "groups_users" ("group_id", "user_id") VALUES (2, 1);
-INSERT INTO "groups_users" ("group_id", "user_id") VALUES (2, 2);
-INSERT INTO "groups_users" ("group_id", "user_id") VALUES (2, 3);
-
-DROP TABLE IF EXISTS "profiles";
-CREATE TABLE "profiles" (
-  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-  "user_id" integer NOT NULL,
-  "interests" text NOT NULL
-);
-
-INSERT INTO "profiles" ("id", "user_id", "interests") VALUES (1, 1, 'music');
-INSERT INTO "profiles" ("id", "user_id", "interests") VALUES (2, 2, 'movies');
-INSERT INTO "profiles" ("id", "user_id", "interests") VALUES (3, 3, 'hockey');
+------------------------------------------------------------
+-- USERS
+------------------------------------------------------------
 
 DROP TABLE IF EXISTS "users";
 CREATE TABLE "users" (
@@ -54,6 +14,88 @@ INSERT INTO "users" ("id", "created_at", "username", "email") VALUES (1, '2014-0
 INSERT INTO "users" ("id", "created_at", "username", "email") VALUES (2, '2014-04-30 14:02:43', 'bar', 'bar@example.org');
 INSERT INTO "users" ("id", "created_at", "username", "email") VALUES (3, '2014-04-30 14:12:43', 'baz', 'baz@example.org');
 
+------------------------------------------------------------
+-- PROFILES
+------------------------------------------------------------
+
+DROP TABLE IF EXISTS "profiles";
+CREATE TABLE "profiles" (
+  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "user_id" integer NOT NULL,
+  "interests" text NOT NULL
+);
+
+INSERT INTO "profiles" ("id", "user_id", "interests") VALUES (1, 1, 'music');
+INSERT INTO "profiles" ("id", "user_id", "interests") VALUES (2, 2, 'movies');
+INSERT INTO "profiles" ("id", "user_id", "interests") VALUES (3, 3, 'hockey');
+
+------------------------------------------------------------
+-- GROUPS
+------------------------------------------------------------
+
+DROP TABLE IF EXISTS "groups";
+CREATE TABLE "groups" (
+  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "name" text NOT NULL
+);
+
+INSERT INTO "groups" ("id", "name") VALUES (1, 'admin');
+INSERT INTO "groups" ("id", "name") VALUES (2, 'user');
+
+------------------------------------------------------------
+-- GROUPS_USERS
+------------------------------------------------------------
+
+DROP TABLE IF EXISTS "groups_users";
+CREATE TABLE "groups_users" (
+  "group_id" integer NOT NULL,
+  "user_id" integer NOT NULL
+);
+
+INSERT INTO "groups_users" ("group_id", "user_id") VALUES (1, 1);
+INSERT INTO "groups_users" ("group_id", "user_id") VALUES (2, 1);
+INSERT INTO "groups_users" ("group_id", "user_id") VALUES (2, 2);
+INSERT INTO "groups_users" ("group_id", "user_id") VALUES (2, 3);
+
+------------------------------------------------------------
+-- ARTICLES
+------------------------------------------------------------
+
+DROP TABLE IF EXISTS "articles";
+CREATE TABLE "articles" (
+  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "user_id" integer NOT NULL,
+  "created_at" text NOT NULL,
+  "title" text NOT NULL,
+  "body" text NOT NULL
+);
+
+INSERT INTO "articles" ("id", "user_id", "created_at", "title", "body") VALUES (1, 1, '2014-04-30 15:02:10', 'article 1', 'article body 1');
+INSERT INTO "articles" ("id", "user_id", "created_at", "title", "body") VALUES (2, 1, '2014-04-30 15:02:10', 'article 2', 'article body 2');
+INSERT INTO "articles" ("id", "user_id", "created_at", "title", "body") VALUES (3, 2, '2014-04-30 15:32:10', 'article 3', 'article body 3');
+
+------------------------------------------------------------
+-- ARTICLE_COMMENTS
+------------------------------------------------------------
+
+DROP TABLE IF EXISTS "article_comments";
+CREATE TABLE "article_comments" (
+  "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "article_id" integer NOT NULL,
+  "user_id" integer NOT NULL,
+  "created_at" text NOT NULL,
+  "comment" text NOT NULL
+);
+
+INSERT INTO "article_comments" ("id", "article_id", "user_id", "created_at", "comment") VALUES (1, 1, 1, '2014-04-30 15:02:10', "article 1 comment 1");
+INSERT INTO "article_comments" ("id", "article_id", "user_id", "created_at", "comment") VALUES (2, 1, 2, '2014-04-30 15:02:10', "article 1 comment 2");
+INSERT INTO "article_comments" ("id", "article_id", "user_id", "created_at", "comment") VALUES (3, 2, 1, '2014-04-30 15:02:10', "article 2 comment 1");
+INSERT INTO "article_comments" ("id", "article_id", "user_id", "created_at", "comment") VALUES (4, 3, 3, '2014-04-30 15:02:10', "article 3 comment 1");
+
+------------------------------------------------------------
+-- OPTIMISTIC_LOCKS
+------------------------------------------------------------
+
 DROP TABLE IF EXISTS "optimistic_locks";
 CREATE TABLE "optimistic_locks" (
   "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -63,17 +105,29 @@ CREATE TABLE "optimistic_locks" (
 
 INSERT INTO "optimistic_locks" ("id", "lock_version", "value") VALUES (1, 0, "foo");
 
+------------------------------------------------------------
+-- UUID_KEYS
+------------------------------------------------------------
+
 DROP TABLE IF EXISTS "uuid_keys";
 CREATE TABLE "uuid_keys" (
   "id" text NOT NULL PRIMARY KEY,
   "value" text NOT NULL
 );
 
+------------------------------------------------------------
+-- CUSTOM_KEYS
+------------------------------------------------------------
+
 DROP TABLE IF EXISTS "custom_keys";
 CREATE TABLE "custom_keys" (
   "id" text NOT NULL PRIMARY KEY,
   "value" text NOT NULL
 );
+
+------------------------------------------------------------
+-- NO_KEYS
+------------------------------------------------------------
 
 DROP TABLE IF EXISTS "no_keys";
 CREATE TABLE "no_keys" (
