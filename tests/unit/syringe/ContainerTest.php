4b2fb91d84d2e92120569e888a8ef9a8936dbcf2
Frederic G. Østby <frederic.g.ostby@gmail.com>
2014-05-21 16:01:05 +0200

    Container tests

--- tests/unit/syringe/ContainerTest.php
+++ tests/unit/syringe/ContainerTest.php
@@ -30,6 +30,26 @@ class Bar
 	}
 }
 
+interface StoreInterface
+{
+
+}
+
+class Store implements StoreInterface
+{
+	
+}
+
+class Baz
+{
+	public $store;
+
+	public function __construct(StoreInterface $store)
+	{
+		$this->store = $store;
+	}
+}
+
 // --------------------------------------------------------------------------
 // END CLASSES
 // --------------------------------------------------------------------------
@@ -51,10 +71,130 @@ class ContainerTest extends \PHPUnit_Framework_TestCase
 		$foo = $container->get('mako\tests\unit\syringe\Foo');
 
 		$this->assertInstanceOf('\StdClass', $foo->stdClass);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testParametersFromReflection()
+	{
+		$container = new Container;
 
 		$bar = $container->get('mako\tests\unit\syringe\Bar');
 
 		$this->assertEquals(123, $bar->foo);
 		$this->assertEquals(456, $bar->bar);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testParameters()
+	{
+		$container = new Container;
+
+		$bar = $container->get('mako\tests\unit\syringe\Bar', ['abc', 'def']);
+
+		$this->assertEquals('abc', $bar->foo);
+		$this->assertEquals('def', $bar->bar);
+
+		//
+
+		$container = new Container;
+
+		$bar = $container->get('mako\tests\unit\syringe\Bar', [1 => 'def', 0 => 'abc']);
+
+		$this->assertEquals('abc', $bar->foo);
+		$this->assertEquals('def', $bar->bar);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testImplementationInjection()
+	{
+		$container = new Container;
+
+		$container->register('mako\tests\unit\syringe\StoreInterface', 'mako\tests\unit\syringe\Store');
+
+		$baz = $container->get('mako\tests\unit\syringe\Baz');
+
+		$this->assertInstanceOf('mako\tests\unit\syringe\Store', $baz->store);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testGetUsingAlias()
+	{
+		$container = new Container;
+
+		$container->register(['mako\tests\unit\syringe\Foo', 'foo'], 'mako\tests\unit\syringe\Foo');
+
+		$foo = $container->get('foo');
+
+		$this->assertInstanceOf('mako\tests\unit\syringe\Foo', $foo);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testRegisterClosure()
+	{
+		$container = new Container;
+
+		$container->register(['mako\tests\unit\syringe\Bar', 'bar'], function()
+		{
+			return new Bar('uvw', 'xyz');
+		});
+
+		$bar = $container->get('bar');
+
+		$this->assertInstanceOf('mako\tests\unit\syringe\Bar', $bar);
+
+		$this->assertEquals('uvw', $bar->foo);
+		$this->assertEquals('xyz', $bar->bar);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testHas()
+	{
+		$container = new Container;
+
+		$container->register(['mako\tests\unit\syringe\Foo', 'foo'], 'mako\tests\unit\syringe\Foo');
+
+		$this->assertTrue($container->has('mako\tests\unit\syringe\Foo'));
+
+		$this->assertTrue($container->has('foo'));
+
+		$this->assertFalse($container->has('mako\tests\unit\syringe\Bar'));
+
+		$this->assertFalse($container->has('bar'));
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testRegisterSingleton()
+	{
+		$container = new Container;
+
+		$container->registerSingleton(['mako\tests\unit\syringe\Bar', 'bar'], function()
+		{
+			return new Bar(uniqid(), uniqid());
+		});
+
+		$this->assertEquals($container->get('bar'), $container->get('bar'));
+
+		$this->assertNotEquals($container->get('bar'), $container->getFresh('bar'));
 	}
 }
