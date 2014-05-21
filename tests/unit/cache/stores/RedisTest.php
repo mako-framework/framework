37b5eb347b73abf7a10d8eeb59b75118e41ec1df
Frederic G. Østby <frederic.g.ostby@gmail.com>
2014-05-21 16:39:30 +0200

    Redis cache store tests

---
+++ tests/unit/cache/stores/RedisTest.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace mako\tests\unit\cache\stores;
+
+use \mako\cache\stores\Redis;
+
+use \Mockery as m;
+
+/**
+ * @group unit
+ */
+
+class ArrTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * 
+	 */
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	/**
+	 * 
+	 */
+
+	public function getRedisClient()
+	{
+		return m::mock('mako\redis\Redis');
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testPut()
+	{
+		$client = $this->getRedisClient();
+
+		$client->shouldReceive('set')->once()->with('foo', 123);
+
+		$redis = new Redis($client);
+
+		$redis->put('foo', 123);
+
+		//
+
+		$client = $this->getRedisClient();
+
+		$client->shouldReceive('set')->once()->with('foo', serialize('foo'));
+
+		$redis = new Redis($client);
+
+		$redis->put('foo', 'foo');
+
+		//
+
+		$client = $this->getRedisClient();
+
+		$client->shouldReceive('set')->once()->with('foo', 123);
+
+		$client->shouldReceive('expire')->once()->with('foo', 3600);
+
+		$redis = new Redis($client);
+
+		$redis->put('foo', 123, 3600);
+
+		//
+
+		$client = $this->getRedisClient();
+
+		$client->shouldReceive('set')->once()->with('foo', serialize('foo'));
+
+		$client->shouldReceive('expire')->once()->with('foo', 3600);
+
+		$redis = new Redis($client);
+
+		$redis->put('foo', 'foo', 3600);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testHas()
+	{
+		$client = $this->getRedisClient();
+
+		$client->shouldReceive('exists')->once()->with('foo');
+
+		$redis = new Redis($client);
+
+		$redis->has('foo');
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testGet()
+	{
+		$client = $this->getRedisClient();
+
+		$client->shouldReceive('get')->once()->with('foo')->andReturn(123);
+
+		$redis = new Redis($client);
+
+		$cached = $redis->get('foo');
+
+		$this->assertEquals(123, $cached);
+
+		//
+
+		$client = $this->getRedisClient();
+
+		$client->shouldReceive('get')->once()->with('foo')->andReturn(serialize('foo'));
+
+		$redis = new Redis($client);
+
+		$cached = $redis->get('foo');
+
+		$this->assertEquals('foo', $cached);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testRemove()
+	{
+		$client = $this->getRedisClient();
+
+		$client->shouldReceive('del')->once()->with('foo');
+
+		$redis = new Redis($client);
+
+		$redis->remove('foo');
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testClear()
+	{
+		$client = $this->getRedisClient();
+
+		$client->shouldReceive('flushdb')->once();
+
+		$redis = new Redis($client);
+
+		$redis->clear();
+	}
+}
