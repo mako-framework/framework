d54d789be14fd09c2fe3ec7b9db8a1fcb5d47bd2
Frederic G. Østby <frederic.g.ostby@gmail.com>
2014-05-23 15:42:47 +0200

    Added OpenSSL and Mcrypt tests

---
+++ tests/unit/security/crypto/encrypters/McryptTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace mako\tests\unit\security\crypto\encrypters;
+
+use \mako\security\crypto\padders\PKCS7;
+use \mako\security\crypto\encrypters\Mcrypt;
+
+/**
+ * @group unit
+ * @requires extension mcrypt
+ */
+
+class McryptTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * 
+	 */
+
+	public function testEncrypt()
+	{
+		$string = 'hello, world!';
+
+		$mcrypt = new Mcrypt('uE4cJ8YzUMev*aAuZBXezXqWr[sNwK', new PKCS7);
+
+		$encrypted1 = $mcrypt->encrypt($string);
+
+		$encrypted2 = $mcrypt->encrypt($string);
+
+		$this->assertNotEquals($string, $encrypted1);
+
+		$this->assertNotEquals($string, $encrypted2);
+
+		$this->assertNotEquals($encrypted1, $encrypted2);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testDecrypt()
+	{
+		$string = 'hello, world!';
+		
+		$mcrypt = new Mcrypt('uE4cJ8YzUMev*aAuZBXezXqWr[sNwK', new PKCS7);
+
+		$encrypted = $mcrypt->encrypt($string);
+
+		$this->assertNotEquals($string, $encrypted);
+
+		$decrypted = $mcrypt->decrypt($encrypted);
+
+		$this->assertEquals($string, $decrypted);
+	}
+}
---
+++ tests/unit/security/crypto/encrypters/OpenSSLTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace mako\tests\unit\security\crypto\encrypters;
+
+use \mako\security\crypto\encrypters\OpenSSL;
+
+/**
+ * @group unit
+ * @requires extension openssl
+ */
+
+class OpenSSLTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * 
+	 */
+
+	public function testEncrypt()
+	{
+		$string = 'hello, world!';
+
+		$openSSL = new OpenSSL('uE4cJ8YzUMev*aAuZBXezXqWr[sNwK');
+
+		$encrypted1 = $openSSL->encrypt($string);
+
+		$encrypted2 = $openSSL->encrypt($string);
+
+		$this->assertNotEquals($string, $encrypted1);
+
+		$this->assertNotEquals($string, $encrypted2);
+
+		$this->assertNotEquals($encrypted1, $encrypted2);
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testDecrypt()
+	{
+		$string = 'hello, world!';
+		
+		$openSSL = new OpenSSL('uE4cJ8YzUMev*aAuZBXezXqWr[sNwK');
+
+		$encrypted = $openSSL->encrypt($string);
+
+		$this->assertNotEquals($string, $encrypted);
+
+		$decrypted = $openSSL->decrypt($encrypted);
+
+		$this->assertEquals($string, $decrypted);
+	}
+}
