89bbb95d7b90a6b44d241f253c492b0eeec6424f
Frederic G. Østby <frederic.g.ostby@gmail.com>
2014-06-09 00:57:33 +0200

    Added more tests

--- tests/unit/security/ComparerTest.php
+++ tests/unit/security/ComparerTest.php
@@ -17,9 +17,25 @@ class ComparerTest extends \PHPUnit_Framework_TestCase
 	public function testCompare()
 	{
 		$this->assertTrue(Comparer::compare('foo', 'foo'));
-		
+
+		$this->assertTrue(Comparer::compare('', ''));
+
+		$this->assertTrue(Comparer::compare(123, 123));
+
+		$this->assertTrue(Comparer::compare(123, '123'));
+
+		$this->assertTrue(Comparer::compare(null, null));
+
+		$this->assertTrue(Comparer::compare(null, ''));
+
 		$this->assertFalse(Comparer::compare('foo', 'bar'));
 
+		$this->assertFalse(Comparer::compare('foo', ''));
+
+		$this->assertFalse(Comparer::compare('', 'foo'));
+
 		$this->assertFalse(Comparer::compare('foo', 'fooo'));
+
+		$this->assertFalse(Comparer::compare('fooo', 'foo'));
 	}
 }
