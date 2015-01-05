02b78ad9bc32c62663ff3025c6e9a6153444a9ed
Frederic G. Østby <frederic.g.ostby@gmail.com>
2015-01-05 16:04:23 +0100

    Added reader implementation

---
+++ src/mako/cli/input/reader/Reader.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @copyright  Frederic G. Østby
+ * @license    http://www.makoframework.com/license
+ */
+
+namespace mako\cli\input\reader;
+
+use mako\cli\input\reader\ReaderInterface;
+
+/**
+ * Reader.
+ *
+ * @author  Frederic G. Østby
+ */
+
+class Reader implements ReaderInterface
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	 
+	public function read()
+	{
+		return trim(fgets(STDIN));
+	}
+}
