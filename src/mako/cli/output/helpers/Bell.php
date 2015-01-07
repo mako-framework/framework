dcaaaf34f07ab4ef92ac79e17f1fe23108db6f65
Frederic G. Østby <frederic.g.ostby@gmail.com>
2015-01-07 16:20:44 +0100

    Added bell helper along with tests

---
+++ src/mako/cli/output/helpers/Bell.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @copyright  Frederic G. Østby
+ * @license    http://www.makoframework.com/license
+ */
+
+namespace mako\cli\output\helpers;
+
+use mako\cli\output\Output;
+
+/**
+ * Terminal bell helper.
+ *
+ * @author  Frederic G. Østby
+ */
+
+class Bell
+{
+	/**
+	 * Output instance.
+	 * 
+	 * @var \mako\cli\output\Output
+	 */
+
+	protected $output;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @access  public
+	 * @param   \mako\cli\output\Output  $output  Output instance
+	 */
+
+	public function __construct(Output $output)
+	{
+		$this->output = $output;
+	}
+
+	/**
+	 * Rings the terminal bell n times.
+	 * 
+	 * @access  public
+	 * @param   int     $times   Number of times to ring the bell
+	 * @param   int     $writer  The writer to use
+	 */
+
+	public function ring($times = 1, $writer = Output::STANDARD)
+	{
+		$this->output->write(str_repeat("\x07", $times), $writer);
+	}
+}
---
+++ tests/unit/cli/output/helpers/BellTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace mako\tests\unit\cli\output\helpers;
+
+use mako\cli\output\helpers\Bell;
+
+use Mockery as m;
+
+/**
+ * @group unit
+ */
+
+class OutputTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * 
+	 */
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testRing()
+	{
+		$output = m::mock('mako\cli\output\Output');
+
+		$output->shouldReceive('write')->once()->with("\x07", 1);
+
+		$bell = new Bell($output);
+
+		$bell->ring();
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testRingMultipleTimes()
+	{
+		$output = m::mock('mako\cli\output\Output');
+
+		$output->shouldReceive('write')->once()->with("\x07\x07\x07", 1);
+
+		$bell = new Bell($output);
+
+		$bell->ring(3);
+	}
+}
