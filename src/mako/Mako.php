60d2cc113304a7b3ca6b5c3488c450086e5df28a
Frederic G. Østby <frederic.g.ostby@gmail.com>
2015-01-10 14:03:51 +0100

    Added Mako core class

---
+++ src/mako/Mako.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @copyright  Frederic G. Østby
+ * @license    http://www.makoframework.com/license
+ */
+
+namespace mako;
+
+/**
+ * Mako.
+ *
+ * @author  Frederic G. Østby
+ */
+
+class Mako
+{
+	/**
+	 * Mako version.
+	 * 
+	 * @var string
+	 */
+
+	const VERSION = '4.4.0';
+
+	/**
+	 * Mako major version.
+	 * 
+	 * @var int
+	 */
+
+	const VERSION_MAJOR = 4;
+
+	/**
+	 * Mako minor version.
+	 * 
+	 * @var int
+	 */
+
+	const VERSION_MINOR = 4;
+
+	/**
+	 * Mako patch version.
+	 * 
+	 * @var int
+	 */
+
+	const VERSION_PATCH = 0;
+}
