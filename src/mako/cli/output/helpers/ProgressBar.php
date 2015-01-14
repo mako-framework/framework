66a54652fd198919703917dd7c69dd59593a0d5d
Frederic G. Østby <frederic.g.ostby@gmail.com>
2015-01-14 10:32:43 +0100

    Added progress bar helper along with tests

---
+++ src/mako/cli/output/helpers/ProgressBar.php
@@ -0,0 +1,176 @@
+<?php
+
+/**
+ * @copyright  Frederic G. Østby
+ * @license    http://www.makoframework.com/license
+ */
+
+namespace mako\cli\output\helpers;
+
+use mako\cli\output\Output;
+
+/**
+ * Progress bar helper.
+ *
+ * @author  Frederic G. Østby
+ */
+
+class ProgressBar
+{
+	/**
+	 * String that represents the empty part of the progess bar.
+	 * 
+	 * @var string
+	 */
+
+	protected $emptyTemplate = '-';
+
+	/**
+	 * String that represents the filled part of the progess bar.
+	 * 
+	 * @var string
+	 */
+
+	protected $filledTemplate = '=';
+
+	/**
+	 * Number of items.
+	 * 
+	 * @var int
+	 */
+
+	protected $items;
+
+	/**
+	 * Redraw rate.
+	 * 
+	 * @var int
+	 */
+
+	protected $redrawRate;
+
+	/**
+	 * Progress status.
+	 * 
+	 * @var int
+	 */
+
+	protected $progress = 0;
+
+	/**
+	 * Output instance.
+	 * 
+	 * @var \mako\cli\output\Output
+	 */
+
+	protected $output;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @access  public
+	 * @param   \mako\cli\output\Output  $output      Output instance
+	 * @param   int                      $items       Total number of items
+	 * @param   int                      $redrawRate  Redraw rate
+	 */
+
+	public function __construct(Output $output, $items, $redrawRate = null)
+	{
+		$this->output = $output;
+
+		$this->items = $items;
+
+		$this->redrawRate = max($redrawRate ?: ceil(0.01 * $items), 1);
+	}
+
+	/**
+	 * Sets the string that represents the empty part of the progess bar.
+	 * 
+	 * @access  public
+	 * @param   string  $template  Template
+	 */
+
+	public function setEmptyTemplate($template)
+	{
+		$this->emptyTemplate = $template;
+	}
+
+	/**
+	 * Sets the string that represents the filled part of the progess bar.
+	 * 
+	 * @access  public
+	 * @param   string  $template  Template
+	 */
+
+	public function setFilledTemplate($template)
+	{
+		$this->filledTemplate = $template;
+	}
+
+	/**
+	 * Builds the progressbar.
+	 * 
+	 * @access  protected
+	 * @param   int        $percent  Percent to fill
+	 * @return  string
+	 */
+
+	protected function buildProgressBar($percent)
+	{
+		$fill = (int) floor($percent / 5);
+
+		$progressBar  = str_pad($this->progress, strlen($this->items), '0', STR_PAD_LEFT) . '/' . $this->items . ' ';
+
+		$progressBar .= str_repeat($this->filledTemplate, $fill);
+
+		$progressBar .= str_repeat($this->emptyTemplate, (20 - $fill));
+
+		$progressBar .= str_pad(' '. $percent . '% ', 6, ' ', STR_PAD_LEFT);
+
+		return $progressBar;
+	}
+
+	/**
+	 * Draws the progressbar.
+	 * 
+	 * @access  public
+	 */
+
+	public function draw()
+	{
+		// Calculate percent
+		
+		$percent = (int) ceil(min(($this->progress / $this->items) * 100, 100));
+
+		// Build progress bar
+
+		$progressBar = $this->buildProgressBar($percent);
+
+		// Draw progressbar
+
+		$this->output->write("\r" . $progressBar);
+
+		// If we're done then we'll add a newline to the output
+
+		if($this->progress === $this->items)
+		{
+			$this->output->write(PHP_EOL);
+		}
+	}
+
+	/**
+	 * Move progress forward and redraws the progessbar.
+	 * 
+	 * @access  public
+	 */
+
+	public function advance()
+	{
+		$this->progress++;
+
+		if($this->progress === $this->items || ($this->progress % $this->redrawRate) === 0)
+		{
+			$this->draw();
+		}
+	}
+}
---
+++ tests/unit/cli/output/helpers/ProgressBarTest.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace mako\tests\unit\cli\output\helpers;
+
+use mako\cli\output\helpers\ProgressBar;
+
+use Mockery as m;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @group unit
+ */
+
+class ProgressBarTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * 
+	 */
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testBasicProgress()
+	{
+		$output = m::mock('mako\cli\output\Output');
+
+		$output->shouldReceive('write')->once()->with("\r00/10 --------------------   0% ");
+		$output->shouldReceive('write')->once()->with("\r01/10 ==------------------  10% ");
+		$output->shouldReceive('write')->once()->with("\r02/10 ====----------------  20% ");
+		$output->shouldReceive('write')->once()->with("\r03/10 ======--------------  30% ");
+		$output->shouldReceive('write')->once()->with("\r04/10 ========------------  40% ");
+		$output->shouldReceive('write')->once()->with("\r05/10 ==========----------  50% ");
+		$output->shouldReceive('write')->once()->with("\r06/10 ============--------  60% ");
+		$output->shouldReceive('write')->once()->with("\r07/10 ==============------  70% ");
+		$output->shouldReceive('write')->once()->with("\r08/10 ================----  80% ");
+		$output->shouldReceive('write')->once()->with("\r09/10 ==================--  90% ");
+		$output->shouldReceive('write')->once()->with("\r10/10 ==================== 100% ");
+		$output->shouldReceive('write')->once()->with(PHP_EOL);
+
+		$progressBar = new ProgressBar($output, 10);
+
+		$progressBar->draw();
+
+		for($i = 0; $i < 10; $i++)
+		{
+			$progressBar->advance();
+		}
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testProgressWithCustomTemplates()
+	{
+		$output = m::mock('mako\cli\output\Output');
+
+		$output->shouldReceive('write')->once()->with("\r00/10 ____________________   0% ");
+		$output->shouldReceive('write')->once()->with("\r01/10 ++__________________  10% ");
+		$output->shouldReceive('write')->once()->with("\r02/10 ++++________________  20% ");
+		$output->shouldReceive('write')->once()->with("\r03/10 ++++++______________  30% ");
+		$output->shouldReceive('write')->once()->with("\r04/10 ++++++++____________  40% ");
+		$output->shouldReceive('write')->once()->with("\r05/10 ++++++++++__________  50% ");
+		$output->shouldReceive('write')->once()->with("\r06/10 ++++++++++++________  60% ");
+		$output->shouldReceive('write')->once()->with("\r07/10 ++++++++++++++______  70% ");
+		$output->shouldReceive('write')->once()->with("\r08/10 ++++++++++++++++____  80% ");
+		$output->shouldReceive('write')->once()->with("\r09/10 ++++++++++++++++++__  90% ");
+		$output->shouldReceive('write')->once()->with("\r10/10 ++++++++++++++++++++ 100% ");
+		$output->shouldReceive('write')->once()->with(PHP_EOL);
+
+		$progressBar = new ProgressBar($output, 10);
+
+		$progressBar->setEmptyTemplate('_');
+
+		$progressBar->setFilledTemplate('+');
+
+		$progressBar->draw();
+
+		for($i = 0; $i < 10; $i++)
+		{
+			$progressBar->advance();
+		}
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testProgressWith100ItemsAndDefaultRedrawRate()
+	{
+		$output = m::mock('mako\cli\output\Output');
+
+		$output->shouldReceive('write')->times(102);
+
+		$progressBar = new ProgressBar($output, 100);
+
+		$progressBar->draw();
+
+		for($i = 0; $i < 100; $i++)
+		{
+			$progressBar->advance();
+		}
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testProgressWith1000ItemsAndDefaultRedrawRate()
+	{
+		$output = m::mock('mako\cli\output\Output');
+
+		$output->shouldReceive('write')->times(102);
+
+		$progressBar = new ProgressBar($output, 1000);
+
+		$progressBar->draw();
+
+		for($i = 0; $i < 1000; $i++)
+		{
+			$progressBar->advance();
+		}
+	}
+
+	/**
+	 * 
+	 */
+
+	public function testProgressWith1000ItemsAndCustomtRedrawRate()
+	{
+		$output = m::mock('mako\cli\output\Output');
+
+		$output->shouldReceive('write')->times(1002);
+
+		$progressBar = new ProgressBar($output, 1000, 1);
+
+		$progressBar->draw();
+
+		for($i = 0; $i < 1000; $i++)
+		{
+			$progressBar->advance();
+		}
+	}
+}
