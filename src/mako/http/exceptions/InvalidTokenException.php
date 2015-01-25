7893e2892aa9382fd1c0631e4d605db5c680c905
Frederic G. Østby <frederic.g.ostby@gmail.com>
2015-01-25 20:43:02 +0100

    Added a InvalidTokenException

---
+++ src/mako/http/exceptions/InvalidTokenException.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @copyright  Frederic G. Østby
+ * @license    http://www.makoframework.com/license
+ */
+
+namespace mako\http\exceptions;
+
+use Exception;
+
+use mako\http\exceptions\RequestException;
+
+/**
+ * Invalid token exception.
+ *
+ * @author  Frederic G. Østby
+ */
+
+class InvalidTokenException extends RequestException
+{
+	/**
+	 * Constructor.
+	 * 
+	 * @access  public
+	 * @param   string  $message      Exception message
+	 * @param   \Exception $previous  Previous exception
+	 */
+
+	public function __construct($message = null, Exception $previous = null)
+	{
+		parent::__construct(498, $message, $previous);
+	}
+}
