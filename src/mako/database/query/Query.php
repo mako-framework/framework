90586de8ecef76d2951a01a225729b81ad61da4b
Frederic G. Østby <frederic.g.ostby@gmail.com>
2015-11-19 14:04:46 +0100

    Cleaning up the query builder

--- src/mako/database/query/Query.php
+++ src/mako/database/query/Query.php
@@ -10,10 +10,10 @@ namespace mako\database\query;
 use Closure;
 
 use mako\database\connections\Connection;
-use mako\database\query\Raw;
 use mako\database\query\Join;
+use mako\database\query\QueryConvenienceTrait;
+use mako\database\query\Raw;
 use mako\database\query\Subquery;
-use mako\pagination\Pagination;
 
 /**
  * Query builder.
@@ -23,6 +23,8 @@ use mako\pagination\Pagination;
 
 class Query
 {
+	use QueryConvenienceTrait;
+
 	/**
 	 * Fetch mode.
 	 *
@@ -428,116 +430,6 @@ class Query
 	}
 
 	/**
-	 * Adds a WHERE equals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function eq($column, $value)
-	{
-		return $this->where($column, '=', $value);
-	}
-
-	/**
-	 * Adds a WHERE not equals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function notEq($column, $value)
-	{
-		return $this->where($column, '<>', $value);
-	}
-
-	/**
-	 * Adds a WHERE less than clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function lt($column, $value)
-	{
-		return $this->where($column, '<', $value);
-	}
-
-	/**
-	 * Adds a WHERE less than or equals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function lte($column, $value)
-	{
-		return $this->where($column, '<=', $value);
-	}
-
-	/**
-	 * Adds a WHERE greater than clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function gt($column, $value)
-	{
-		return $this->where($column, '>', $value);
-	}
-
-	/**
-	 * Adds a WHERE greater than or requals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function gte($column, $value)
-	{
-		return $this->where($column, '>=', $value);
-	}
-
-	/**
-	 * Adds a WHERE like clause.
-	 *
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function like($column, $value)
-	{
-		return $this->where($column, 'LIKE', $value);
-	}
-
-	/**
-	 * Adds a WHERE not like clause.
-	 *
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function notLike($column, $value)
-	{
-		return $this->where($column, 'NOT LIKE', $value);
-	}
-
-	/**
 	 * Adds a raw WHERE clause
 	 *
 	 * @access  public
@@ -551,116 +443,6 @@ class Query
 	public function whereRaw($column, $operator, $raw, $separator = 'AND')
 	{
 		return $this->where($column, $operator, new Raw($raw), $separator);
-	}
-
-	/**
-	 * Adds a raw WHERE equals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function eqRaw($column, $value)
-	{
-		return $this->whereRaw($column, '=', $value);
-	}
-
-	/**
-	 * Adds a raw WHERE not equals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function notEqRaw($column, $value)
-	{
-		return $this->whereRaw($column, '<>', $value);
-	}
-
-	/**
-	 * Adds a raw WHERE less than clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function ltRaw($column, $value)
-	{
-		return $this->whereRaw($column, '<', $value);
-	}
-
-	/**
-	 * Adds a raw WHERE less than or equals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function lteRaw($column, $value)
-	{
-		return $this->whereRaw($column, '<=', $value);
-	}
-
-	/**
-	 * Adds a raw WHERE greater than clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function gtRaw($column, $value)
-	{
-		return $this->whereRaw($column, '>', $value);
-	}
-
-	/**
-	 * Adds a raw WHERE greater than or requals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function gteRaw($column, $value)
-	{
-		return $this->whereRaw($column, '>=', $value);
-	}
-
-	/**
-	 * Adds a raw WHERE like clause.
-	 *
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function likeRaw($column, $value)
-	{
-		return $this->whereRaw($column, 'LIKE', $value);
-	}
-
-	/**
-	 * Adds a raw WHERE not like clause.
-	 *
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function notLikeRaw($column, $value)
-	{
-		return $this->whereRaw($column, 'NOT LIKE', $value);
 	}
 
 	/**
@@ -679,116 +461,6 @@ class Query
 	}
 
 	/**
-	 * Adds a OR WHERE equals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orEq($column, $value)
-	{
-		return $this->orWhere($column, '=', $value);
-	}
-
-	/**
-	 * Adds a OR WHERE not equals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orNotEq($column, $value)
-	{
-		return $this->orWhere($column, '<>', $value);
-	}
-
-	/**
-	 * Adds a OR WHERE less than clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orLt($column, $value)
-	{
-		return $this->orWhere($column, '<', $value);
-	}
-
-	/**
-	 * Adds a OR WHERE less than or requals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orLte($column, $value)
-	{
-		return $this->orWhere($column, '<=', $value);
-	}
-
-	/**
-	 * Adds a OR WHERE greater than clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orGt($column, $value)
-	{
-		return $this->orWhere($column, '>', $value);
-	}
-
-	/**
-	 * Adds a OR WHERE greater than or requals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orGte($column, $value)
-	{
-		return $this->orWhere($column, '>=', $value);
-	}
-
-	/**
-	 * Adds a OR WHERE like clause.
-	 *
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orLike($column, $value)
-	{
-		return $this->orWhere($column, 'LIKE', $value);
-	}
-
-	/**
-	 * Adds a OR WHERE not like clause.
-	 *
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orNotLike($column, $value)
-	{
-		return $this->orWhere($column, 'NOT LIKE', $value);
-	}
-
-	/**
 	 * Adds a raw OR WHERE clause.
 	 *
 	 * @access  public
@@ -801,116 +473,6 @@ class Query
 	public function orWhereRaw($column, $operator, $raw)
 	{
 		return $this->whereRaw($column, $operator, $raw, 'OR');
-	}
-
-	/**
-	 * Adds a raw OR WHERE equals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orEqRaw($column, $value)
-	{
-		return $this->orWhereRaw($column, '=', $value);
-	}
-
-	/**
-	 * Adds a raw OR WHERE not equals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orNotEqRaw($column, $value)
-	{
-		return $this->orWhereRaw($column, '<>', $value);
-	}
-
-	/**
-	 * Adds a raw OR WHERE less than clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orLtRaw($column, $value)
-	{
-		return $this->orWhereRaw($column, '<', $value);
-	}
-
-	/**
-	 * Adds a raw OR WHERE less than or requals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orLteRaw($column, $value)
-	{
-		return $this->orWhereRaw($column, '<=', $value);
-	}
-
-	/**
-	 * Adds a raw OR WHERE greater than clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orGtRaw($column, $value)
-	{
-		return $this->orWhereRaw($column, '>', $value);
-	}
-
-	/**
-	 * Adds a OR WHERE greater than or requals clause.
-	 *
-	 * @access  public
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orGteRaw($column, $value)
-	{
-		return $this->orWhereRaw($column, '>=', $value);
-	}
-
-	/**
-	 * Adds a raw OR WHERE like clause.
-	 *
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orLikeRaw($column, $value)
-	{
-		return $this->orWhereRaw($column, 'LIKE', $value);
-	}
-
-	/**
-	 * Adds a raw OR WHERE not like clause.
-	 *
-	 * @param   string                      $column  Column name
-	 * @param   mixed                       $value   Value
-	 * @return  \mako\database\query\Query
-	 */
-
-	public function orNotLikeRaw($column, $value)
-	{
-		return $this->orWhereRaw($column, 'NOT LIKE', $value);
 	}
 
 	/**
@@ -1466,19 +1028,6 @@ class Query
 	}
 
 	/**
-     * Paginates the results using a paniation instance.
-     *
-     * @access  public
-     * @param   \mako\pagination\Pagination  $pagination  Pagination instance
-     * @return  \mako\database\query\Query
-     */
-
-	public function paginate(Pagination $pagination)
-	{
-		return $this->limit($pagination->limit())->offset($pagination->offset());
-	}
-
-	/**
 	 * Executes a SELECT query and returns the value of the chosen column of the first row of the result set.
 	 *
 	 * @access  public
@@ -1692,34 +1241,6 @@ class Query
 		$query = $this->compiler->update($values);
 
 		return $this->connection->queryAndCount($query['sql'], $query['params']);
-	}
-
-	/**
-	 * Increments column value.
-	 *
-	 * @access  public
-	 * @param   string  $column     Column name
-	 * @param   int     $increment  Increment value
-	 * @return  int
-	 */
-
-	public function increment($column, $increment = 1)
-	{
-		return $this->update([$column => new Raw($this->compiler->escapeIdentifier($column) . ' + ' . (int) $increment)]);
-	}
-
-	/**
-	 * Decrements column value.
-	 *
-	 * @access  public
-	 * @param   string  $column     Column name
-	 * @param   int     $decrement  Decrement value
-	 * @return  int
-	 */
-
-	public function decrement($column, $decrement = 1)
-	{
-		return $this->update([$column => new Raw($this->compiler->escapeIdentifier($column) . ' - ' . (int) $decrement)]);
 	}
 
 	/**
---
+++ src/mako/database/query/QueryConvenienceTrait.php
@@ -0,0 +1,500 @@
+<?php
+
+/**
+ * @copyright  Frederic G. Østby
+ * @license    http://www.makoframework.com/license
+ */
+
+namespace mako\database\query;
+
+use mako\pagination\Pagination;
+
+/**
+ * Query builder convenience methods.
+ *
+ * @author  Frederic G. Østby
+ */
+
+trait QueryConvenienceTrait
+{
+	/**
+	 * Adds a WHERE equals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function eq($column, $value)
+	{
+		return $this->where($column, '=', $value);
+	}
+
+	/**
+	 * Adds a WHERE not equals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function notEq($column, $value)
+	{
+		return $this->where($column, '<>', $value);
+	}
+
+	/**
+	 * Adds a WHERE less than clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function lt($column, $value)
+	{
+		return $this->where($column, '<', $value);
+	}
+
+	/**
+	 * Adds a WHERE less than or equals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function lte($column, $value)
+	{
+		return $this->where($column, '<=', $value);
+	}
+
+	/**
+	 * Adds a WHERE greater than clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function gt($column, $value)
+	{
+		return $this->where($column, '>', $value);
+	}
+
+	/**
+	 * Adds a WHERE greater than or requals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function gte($column, $value)
+	{
+		return $this->where($column, '>=', $value);
+	}
+
+	/**
+	 * Adds a WHERE like clause.
+	 *
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function like($column, $value)
+	{
+		return $this->where($column, 'LIKE', $value);
+	}
+
+	/**
+	 * Adds a WHERE not like clause.
+	 *
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function notLike($column, $value)
+	{
+		return $this->where($column, 'NOT LIKE', $value);
+	}
+
+	/**
+	 * Adds a raw WHERE equals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function eqRaw($column, $value)
+	{
+		return $this->whereRaw($column, '=', $value);
+	}
+
+	/**
+	 * Adds a raw WHERE not equals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function notEqRaw($column, $value)
+	{
+		return $this->whereRaw($column, '<>', $value);
+	}
+
+	/**
+	 * Adds a raw WHERE less than clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function ltRaw($column, $value)
+	{
+		return $this->whereRaw($column, '<', $value);
+	}
+
+	/**
+	 * Adds a raw WHERE less than or equals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function lteRaw($column, $value)
+	{
+		return $this->whereRaw($column, '<=', $value);
+	}
+
+	/**
+	 * Adds a raw WHERE greater than clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function gtRaw($column, $value)
+	{
+		return $this->whereRaw($column, '>', $value);
+	}
+
+	/**
+	 * Adds a raw WHERE greater than or requals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function gteRaw($column, $value)
+	{
+		return $this->whereRaw($column, '>=', $value);
+	}
+
+	/**
+	 * Adds a raw WHERE like clause.
+	 *
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function likeRaw($column, $value)
+	{
+		return $this->whereRaw($column, 'LIKE', $value);
+	}
+
+	/**
+	 * Adds a raw WHERE not like clause.
+	 *
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function notLikeRaw($column, $value)
+	{
+		return $this->whereRaw($column, 'NOT LIKE', $value);
+	}
+
+	/**
+	 * Adds a OR WHERE equals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orEq($column, $value)
+	{
+		return $this->orWhere($column, '=', $value);
+	}
+
+	/**
+	 * Adds a OR WHERE not equals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orNotEq($column, $value)
+	{
+		return $this->orWhere($column, '<>', $value);
+	}
+
+	/**
+	 * Adds a OR WHERE less than clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orLt($column, $value)
+	{
+		return $this->orWhere($column, '<', $value);
+	}
+
+	/**
+	 * Adds a OR WHERE less than or requals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orLte($column, $value)
+	{
+		return $this->orWhere($column, '<=', $value);
+	}
+
+	/**
+	 * Adds a OR WHERE greater than clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orGt($column, $value)
+	{
+		return $this->orWhere($column, '>', $value);
+	}
+
+	/**
+	 * Adds a OR WHERE greater than or requals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orGte($column, $value)
+	{
+		return $this->orWhere($column, '>=', $value);
+	}
+
+	/**
+	 * Adds a OR WHERE like clause.
+	 *
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orLike($column, $value)
+	{
+		return $this->orWhere($column, 'LIKE', $value);
+	}
+
+	/**
+	 * Adds a OR WHERE not like clause.
+	 *
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orNotLike($column, $value)
+	{
+		return $this->orWhere($column, 'NOT LIKE', $value);
+	}
+
+	/**
+	 * Adds a raw OR WHERE equals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orEqRaw($column, $value)
+	{
+		return $this->orWhereRaw($column, '=', $value);
+	}
+
+	/**
+	 * Adds a raw OR WHERE not equals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orNotEqRaw($column, $value)
+	{
+		return $this->orWhereRaw($column, '<>', $value);
+	}
+
+	/**
+	 * Adds a raw OR WHERE less than clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orLtRaw($column, $value)
+	{
+		return $this->orWhereRaw($column, '<', $value);
+	}
+
+	/**
+	 * Adds a raw OR WHERE less than or requals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orLteRaw($column, $value)
+	{
+		return $this->orWhereRaw($column, '<=', $value);
+	}
+
+	/**
+	 * Adds a raw OR WHERE greater than clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orGtRaw($column, $value)
+	{
+		return $this->orWhereRaw($column, '>', $value);
+	}
+
+	/**
+	 * Adds a OR WHERE greater than or requals clause.
+	 *
+	 * @access  public
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orGteRaw($column, $value)
+	{
+		return $this->orWhereRaw($column, '>=', $value);
+	}
+
+	/**
+	 * Adds a raw OR WHERE like clause.
+	 *
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orLikeRaw($column, $value)
+	{
+		return $this->orWhereRaw($column, 'LIKE', $value);
+	}
+
+	/**
+	 * Adds a raw OR WHERE not like clause.
+	 *
+	 * @param   string                      $column  Column name
+	 * @param   mixed                       $value   Value
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function orNotLikeRaw($column, $value)
+	{
+		return $this->orWhereRaw($column, 'NOT LIKE', $value);
+	}
+
+	/**
+	 * Paginates the results using a pagination instance.
+	 *
+	 * @access  public
+	 * @param   \mako\pagination\Pagination  $pagination  Pagination instance
+	 * @return  \mako\database\query\Query
+	 */
+
+	public function paginate(Pagination $pagination)
+	{
+		return $this->limit($pagination->limit())->offset($pagination->offset());
+	}
+
+	/**
+	 * Increments column value.
+	 *
+	 * @access  public
+	 * @param   string  $column     Column name
+	 * @param   int     $increment  Increment value
+	 * @return  int
+	 */
+
+	public function increment($column, $increment = 1)
+	{
+		return $this->update([$column => new Raw($this->compiler->escapeIdentifier($column) . ' + ' . (int) $increment)]);
+	}
+
+	/**
+	 * Decrements column value.
+	 *
+	 * @access  public
+	 * @param   string  $column     Column name
+	 * @param   int     $decrement  Decrement value
+	 * @return  int
+	 */
+
+	public function decrement($column, $decrement = 1)
+	{
+		return $this->update([$column => new Raw($this->compiler->escapeIdentifier($column) . ' - ' . (int) $decrement)]);
+	}
+}
