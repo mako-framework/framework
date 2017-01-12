3f100dc8bb5ef24c179ee5a63e63a0c7c67fc702
Frederic G. Østby <frederic.g.ostby@gmail.com>
2017-01-12 23:24:02 +0100

    ORM::getForeignKey() now uses Str::camel2underscored and not strtolower
    It is also possible to configure the foreign key name

--- tests/integration/database/midgard/relations/AutoEagerLoadingTest.php
+++ tests/integration/database/midgard/relations/AutoEagerLoadingTest.php
@@ -63,7 +63,7 @@ class AutoEagerLoadingTest extends \ORMTestCase
 
 		$this->assertEquals('SELECT * FROM "users" ORDER BY "id" ASC', $this->connectionManager->connection('sqlite')->getLog()[0]['query']);
 
-		$this->assertEquals('SELECT * FROM "articles" WHERE "autoeagerloadinguser_id" IN (\'1\', \'2\', \'3\')', $this->connectionManager->connection('sqlite')->getLog()[1]['query']);
+		$this->assertEquals('SELECT * FROM "articles" WHERE "auto_eager_loading_user_id" IN (\'1\', \'2\', \'3\')', $this->connectionManager->connection('sqlite')->getLog()[1]['query']);
 	}
 
 	/**
@@ -89,10 +89,10 @@ class AutoEagerLoadingTest extends \ORMTestCase
 
 		$this->assertEquals('SELECT * FROM "users" ORDER BY "id" ASC', $this->connectionManager->connection('sqlite')->getLog()[0]['query']);
 
-		$this->assertEquals('SELECT * FROM "articles" WHERE "autoeagerloadinguser_id" = \'1\'', $this->connectionManager->connection('sqlite')->getLog()[1]['query']);
+		$this->assertEquals('SELECT * FROM "articles" WHERE "auto_eager_loading_user_id" = \'1\'', $this->connectionManager->connection('sqlite')->getLog()[1]['query']);
 
-		$this->assertEquals('SELECT * FROM "articles" WHERE "autoeagerloadinguser_id" = \'2\'', $this->connectionManager->connection('sqlite')->getLog()[2]['query']);
+		$this->assertEquals('SELECT * FROM "articles" WHERE "auto_eager_loading_user_id" = \'2\'', $this->connectionManager->connection('sqlite')->getLog()[2]['query']);
 
-		$this->assertEquals('SELECT * FROM "articles" WHERE "autoeagerloadinguser_id" = \'3\'', $this->connectionManager->connection('sqlite')->getLog()[3]['query']);
+		$this->assertEquals('SELECT * FROM "articles" WHERE "auto_eager_loading_user_id" = \'3\'', $this->connectionManager->connection('sqlite')->getLog()[3]['query']);
 	}
 }
