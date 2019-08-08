c61526042ae5a011d3adda8ddfd3c8c25abb4b70
Frederic G. Østby <frederic.g.ostby@gmail.com>
2019-08-08 19:07:08 +0200

    Bugfix

--- src/mako/Mako.php
+++ src/mako/Mako.php
@@ -19,7 +19,7 @@ class Mako
 	 *
 	 * @var string
 	 */
-	const VERSION = '5.7.2';
+	const VERSION = '5.7.3';
 
 	/**
 	 * Mako major version.
@@ -40,5 +40,5 @@ class Mako
 	 *
 	 * @var int
 	 */
-	const VERSION_PATCH = 2;
+	const VERSION_PATCH = 3;
 }
