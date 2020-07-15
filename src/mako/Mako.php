208fd45b8b6a05b77b07450d5b6af2a4c7f2d0ea
Frederic G. Østby <frederic.g.ostby@gmail.com>
2020-07-15 11:02:53 +0200

    Bumped version

--- src/mako/Mako.php
+++ src/mako/Mako.php
@@ -19,7 +19,7 @@ class Mako
 	 *
 	 * @var string
 	 */
-	const VERSION = '6.3.10';
+	const VERSION = '6.3.11';
 
 	/**
 	 * Mako major version.
@@ -40,5 +40,5 @@ class Mako
 	 *
 	 * @var int
 	 */
-	const VERSION_PATCH = 10;
+	const VERSION_PATCH = 11;
 }
