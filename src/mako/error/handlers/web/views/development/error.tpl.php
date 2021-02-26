791c28253438ec1c9a80de43b99bab63f6eb6eb0
Frederic G. Østby <frederic.g.ostby@gmail.com>
2021-02-26 01:50:36 +0100

    New error view (WIP)

--- src/mako/error/handlers/web/views/development/error.tpl.php
+++ src/mako/error/handlers/web/views/development/error.tpl.php
@@ -84,6 +84,13 @@
 				background-color: #123;
 				color: #2DB28A;
 			}
+			.exception > .body.details > .frame > .title > .toggle {
+				background-color: #fff;
+				color: #666;
+				padding: .25rem;
+				border-radius: 4px;
+				float: right;
+			}
 			.exception > .body.details > .frame > .details {
 				background-color: #FFFFFF;
 				border-top: 1px solid #CCCCCC;
@@ -124,7 +131,7 @@
 				<div class="environment">Mako: {{\mako\Mako::VERSION}}, PHP: {{PHP_VERSION}}</div>
 			</div>
 			<div class="body">
-				<h1>{{$type}} (<span title="Exception code">{{$code}}</span>)</h1>
+				<h1>{{$type}} ({{$code}})</h1>
 
 				<p>{{rtrim($message, '.')}}.</p>
 
@@ -137,6 +144,7 @@
 				{% foreach($trace as $key => $frame) %}
 					<div class="frame">
 						<div class="title">
+							<span class="toggle" aria-hidden="true">{{$frame['open'] ? '▼' : '▲'}}</span>
 							<span class="number">{{$key}}</span>
 							<span class="type {{$frame['is_app'] ? 'app' : 'vendor'}}">{{$frame['is_internal'] ? 'Internal' : ($frame['is_app'] ? 'App' : 'Vendor')}}</span>
 							{{$frame['class'], default: ''}}{{$frame['type'], default: ''}}<span class="function">{{$frame['function']}}()</span>
@@ -172,6 +180,8 @@
 				document.querySelectorAll('.frame > .title').forEach(function(element) {
 					element.addEventListener('click', function() {
 						element.nextElementSibling.style.display = element.nextElementSibling.style.display === 'none' ? 'block' : 'none';
+
+						element.querySelector('.toggle').innerHTML = element.nextElementSibling.style.display === 'none' ? '▲' : '▼';
 					});
 				});
 			});
