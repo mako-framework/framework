75ac7fc228d5a8c0fd820281af0f65dca4ca0d82
Frederic G. Østby <frederic.g.ostby@gmail.com>
2021-11-28 11:03:34 +0100

    PHP 8.1 compatibility

--- src/mako/Mako.php
+++ src/mako/Mako.php
@@ -19,7 +19,7 @@ class Mako
 	 *
 	 * @var string
 	 */
-	public const VERSION = '6.3.18';
+	public const VERSION = '6.3.19';
 
 	/**
 	 * Mako major version.
@@ -40,5 +40,5 @@ class Mako
 	 *
 	 * @var int
 	 */
-	public const VERSION_PATCH = 18;
+	public const VERSION_PATCH = 19;
 }
