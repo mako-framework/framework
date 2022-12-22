e5ea85612d396fec0c328cd84f61e55ca09dc1b7
Frederic G. Østby <frederic.g.ostby@gmail.com>
2022-12-22 19:45:25 +0100

    Prepare branch for 9.1

--- src/mako/Mako.php
+++ src/mako/Mako.php
@@ -17,7 +17,7 @@ class Mako
 	 *
 	 * @var string
 	 */
-	public const VERSION = '9.0.0';
+	public const VERSION = '9.1.0';
 
 	/**
 	 * Mako major version.
@@ -31,7 +31,7 @@ class Mako
 	 *
 	 * @var int
 	 */
-	public const VERSION_MINOR = 0;
+	public const VERSION_MINOR = 1;
 
 	/**
 	 * Mako patch version.
