e2516940fa736b4af04dbbf5d2fb7d6988170073
Frederic G. Østby <frederic.g.ostby@gmail.com>
2023-09-06 21:52:02 +0200

    Create StatusTest.php

---
+++ tests/unit/http/response/StatusTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @copyright Frederic G. Østby
+ * @license   http://www.makoframework.com/license
+ */
+
+namespace mako\tests\unit\http;
+
+use mako\http\response\Status;
+use mako\tests\TestCase;
+use ValueError;
+
+/**
+ * @group unit
+ */
+class StatusTest extends TestCase
+{
+	/**
+	 *
+	 */
+	public function testStatus(): void
+	{
+		$this->assertSame(Status::OK, Status::from(200));
+		$this->assertSame('OK', Status::OK->getMessage());
+	}
+
+	/**
+	 *
+	 */
+	public function testInvalidStatus(): void
+	{
+		$this->expectException(ValueError::class);
+		$this->expectExceptionMessageMatches('/^999 is not a valid backing value for enum/');
+
+		Status::from(999);
+	}
+}
