7b6cc15737e5ab4cbcc3e37fc1aaf2ab40da4853
Frederic G. Østby <frederic.g.ostby@gmail.com>
2024-10-30 00:09:32 +0100

    Update Application.php

--- src/mako/application/web/Application.php
+++ src/mako/application/web/Application.php
@@ -66,6 +66,7 @@ class Application extends BaseApplication
 		if ($this->container->has(DeferredTasks::class)) {
 			if (function_exists('fastcgi_finish_request')) {
 				if ($this->container->has(Session::class)) {
+					/** @var Session $session */
 					$session = $this->container->get(Session::class);
 					$session->disableAutoCommit();
 					$session->commit();
