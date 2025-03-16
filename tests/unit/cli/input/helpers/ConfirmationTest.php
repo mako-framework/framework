aaba264eddc7044d021c78ec14dd71456c5a7cb4
Frederic G. Østby <frederic.g.ostby@gmail.com>
2025-03-16 18:20:48 +0100

    Added more tests

--- tests/unit/cli/input/helpers/ConfirmationTest.php
+++ tests/unit/cli/input/helpers/ConfirmationTest.php
@@ -9,7 +9,11 @@ namespace mako\tests\unit\cli\input\helpers;
 
 use mako\cli\Environment;
 use mako\cli\input\helpers\Confirmation;
+use mako\cli\input\helpers\confirmation\AsciiTheme;
+use mako\cli\input\helpers\confirmation\Theme;
 use mako\cli\input\Input;
+use mako\cli\input\Key;
+use mako\cli\output\Cursor;
 use mako\cli\output\Output;
 use mako\tests\TestCase;
 use Mockery;
@@ -46,7 +50,7 @@ class ConfirmationTest extends TestCase
 
 		$confirmation = new Confirmation($input, $output);
 
-		$this->assertSame(true, $confirmation->ask('Delete all files?'));
+		$this->assertTrue($confirmation->ask('Delete all files?'));
 	}
 
 	/**
@@ -77,7 +81,7 @@ class ConfirmationTest extends TestCase
 
 		$confirmation = new Confirmation($input, $output);
 
-		$this->assertSame(false, $confirmation->ask('Delete all files?'));
+		$this->assertFalse($confirmation->ask('Delete all files?'));
 	}
 
 	/**
@@ -108,7 +112,7 @@ class ConfirmationTest extends TestCase
 
 		$confirmation = new Confirmation($input, $output);
 
-		$this->assertSame(false, $confirmation->ask('Delete all files?', false));
+		$this->assertFalse($confirmation->ask('Delete all files?', false));
 	}
 
 	/**
@@ -139,13 +143,13 @@ class ConfirmationTest extends TestCase
 
 		$confirmation = new Confirmation($input, $output);
 
-		$this->assertSame(true, $confirmation->ask('Delete all files?', true));
+		$this->assertTrue($confirmation->ask('Delete all files?', true));
 	}
 
 	/**
 	 *
 	 */
-	public function testNonInteractiveConfirmationYesCustom(): void
+	public function testNonInteractiveConfirmationYesWithCustomLabels(): void
 	{
 		/** @var Input|Mockery\MockInterface $input */
 		$input = Mockery::mock(Input::class);
@@ -170,7 +174,7 @@ class ConfirmationTest extends TestCase
 
 		$confirmation = new Confirmation($input, $output, trueLabel: 'Ja', falseLabel: 'Nei');
 
-		$this->assertSame(true, $confirmation->ask('Delete all files?'));
+		$this->assertTrue($confirmation->ask('Delete all files?'));
 	}
 
 	/**
@@ -202,6 +206,212 @@ class ConfirmationTest extends TestCase
 
 		$confirmation = new Confirmation($input, $output);
 
-		$this->assertSame(true, $confirmation->ask('Delete all files?'));
+		$this->assertTrue($confirmation->ask('Delete all files?'));
+	}
+
+	/**
+	 * @return array{
+	 * 	Mockery\MockInterface|Input,
+	 *	Mockery\MockInterface|Environment,
+	 *  Mockery\MockInterface|Cursor,
+	 * 	Mockery\MockInterface|Output,
+	 * 	Mockery\MockInterface|Confirmation
+	 * }
+	 */
+	protected function getInteractiveMocks(
+		string $trueLabel = 'Yes',
+		string $falseLabel = 'No',
+		Theme $theme = new Theme,
+	): array {
+		/** @var Input|Mockery\MockInterface $input */
+		$input = Mockery::mock(Input::class);
+
+		/** @var Environment|Mockery\MockInterface $environment */
+		$environment = Mockery::mock(Environment::class);
+
+		$environment->shouldReceive('hasStty')->once()->andReturn(true);
+
+		/** @var Cursor|Mockery\MockInterface $cursor */
+		$cursor = Mockery::mock(Cursor::class);
+
+		$cursor->shouldReceive('hide');
+		$cursor->shouldReceive('show');
+		$cursor->shouldReceive('clearScreenFromCursor');
+		$cursor->shouldReceive('up');
+
+		/** @var Mockery\MockInterface|Output $output */
+		$output = Mockery::mock(Output::class);
+
+		(function () use ($environment, $cursor): void {
+			$this->environment = $environment;
+			$this->cursor = $cursor;
+		})->bindTo($output, Output::class)();
+
+		/** @var Confirmation|Mockery\MockInterface $confirmation */
+		$confirmation = Mockery::mock(Confirmation::class, [
+			$input,
+			$output,
+			$trueLabel,
+			$falseLabel,
+			$theme,
+		]);
+
+		$confirmation->makePartial();
+
+		$confirmation->shouldAllowMockingProtectedMethods();
+
+		$confirmation->shouldReceive('getSttySettings');
+		$confirmation->shouldReceive('setSttySettings');
+
+		return [$input, $environment, $cursor, $output, $confirmation];
+	}
+
+	/**
+	 *
+	 */
+	public function testInteractiveConfirmationYes(): void
+	{
+		[$input, , , $output, $confirmation] = $this->getInteractiveMocks();
+
+		$output->shouldReceive('writeLn')->once()->with('Delete all files?');
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+
+		 ○ Yes ● No
+
+		OUTPUT);
+
+		$input->shouldReceive('readBytes')->once()->andReturn(Key::LEFT->value);
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+
+		 ● Yes ○ No
+
+		OUTPUT);
+
+		$input->shouldReceive('readBytes')->once()->andReturn(Key::ENTER->value);
+
+		$this->assertTrue($confirmation->ask('Delete all files?'));
+	}
+
+	/**
+	 *
+	 */
+	public function testInteractiveConfirmationNo(): void
+	{
+		[$input, , , $output, $confirmation] = $this->getInteractiveMocks();
+
+		$output->shouldReceive('writeLn')->once()->with('Delete all files?');
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+
+		 ○ Yes ● No
+
+		OUTPUT);
+
+		$input->shouldReceive('readBytes')->once()->andReturn(Key::ENTER->value);
+
+		$this->assertFalse($confirmation->ask('Delete all files?'));
+	}
+
+	/**
+	 *
+	 */
+	public function testInteractiveConfirmationDefaultNo(): void
+	{
+		[$input, , , $output, $confirmation] = $this->getInteractiveMocks();
+
+		$output->shouldReceive('writeLn')->once()->with('Delete all files?');
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+
+		 ○ Yes ● No
+
+		OUTPUT);
+
+		$input->shouldReceive('readBytes')->once()->andReturn(Key::ENTER->value);
+
+		$this->assertFalse($confirmation->ask('Delete all files?', false));
+	}
+
+	/**
+	 *
+	 */
+	public function testInteractiveConfirmationDefaulYes(): void
+	{
+		[$input, , , $output, $confirmation] = $this->getInteractiveMocks();
+
+		$output->shouldReceive('writeLn')->once()->with('Delete all files?');
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+
+		 ● Yes ○ No
+
+		OUTPUT);
+
+		$input->shouldReceive('readBytes')->once()->andReturn(Key::ENTER->value);
+
+		$this->assertTrue($confirmation->ask('Delete all files?', true));
+	}
+
+	/**
+	 *
+	 */
+	public function testInteractiveConfirmationYesWithCustomLabels(): void
+	{
+		[$input, , , $output, $confirmation] = $this->getInteractiveMocks(
+			trueLabel: 'Ja',
+			falseLabel: 'Nei'
+		);
+
+		$output->shouldReceive('writeLn')->once()->with('Delete all files?');
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+
+		 ○ Ja ● Nei
+
+		OUTPUT);
+
+		$input->shouldReceive('readBytes')->once()->andReturn(Key::LEFT->value);
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+
+		 ● Ja ○ Nei
+
+		OUTPUT);
+
+		$input->shouldReceive('readBytes')->once()->andReturn(Key::ENTER->value);
+
+		$this->assertTrue($confirmation->ask('Delete all files?'));
+	}
+
+	/**
+	 *
+	 */
+	public function testInteractiveConfirmationYesWithCustomTheme(): void
+	{
+		[$input, , , $output, $confirmation] = $this->getInteractiveMocks(
+			theme: new AsciiTheme
+		);
+
+		$output->shouldReceive('writeLn')->once()->with('Delete all files?');
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+
+		 [ ] Yes [X] No
+
+		OUTPUT);
+
+		$input->shouldReceive('readBytes')->once()->andReturn(Key::LEFT->value);
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+
+		 [X] Yes [ ] No
+
+		OUTPUT);
+
+		$input->shouldReceive('readBytes')->once()->andReturn(Key::ENTER->value);
+
+		$this->assertTrue($confirmation->ask('Delete all files?'));
 	}
 }
