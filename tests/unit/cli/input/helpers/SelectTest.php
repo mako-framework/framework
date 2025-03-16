cca99dfe57019970449d4eb0d38c8bbab9c53a89
Frederic G. Østby <frederic.g.ostby@gmail.com>
2025-03-16 10:01:01 +0100

    Added more tests

--- tests/unit/cli/input/helpers/SelectTest.php
+++ tests/unit/cli/input/helpers/SelectTest.php
@@ -21,7 +21,7 @@ class SelectTest extends TestCase
 	/**
 	 *
 	 */
-	public function testNonInteractiveSelectAndPickFirstOptionWithNumericInput(): void
+	public function testNonInteractiveSelectAndPickFirstOption(): void
 	{
 		/** @var Input|Mockery\MockInterface $input */
 		$input = Mockery::mock(Input::class);
@@ -56,42 +56,7 @@ class SelectTest extends TestCase
 	/**
 	 *
 	 */
-	public function testSelectAndPickFirstOptionWithNumericInput(): void
-	{
-		/** @var Input|Mockery\MockInterface $input */
-		$input = Mockery::mock(Input::class);
-
-		$input->shouldReceive('read')->once()->andReturn('1');
-
-		/** @var Environment|Mockery\MockInterface $environment */
-		$environment = Mockery::mock(Environment::class);
-
-		$environment->shouldReceive('hasStty')->once()->andReturn(false);
-
-		/** @var Mockery\MockInterface|Output $output */
-		$output = Mockery::mock(Output::class);
-
-		(function () use ($environment): void {
-			$this->environment = $environment;
-		})->bindTo($output, Output::class)();
-
-		$output->shouldReceive('writeLn')->once()->with('Favorite food?');
-
-		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
-		1) Burgers
-		2) Sushi
-		>
-		OUTPUT . ' ');
-
-		$select = new Select($input, $output);
-
-		$this->assertSame(0, $select->ask('Favorite food?', ['Burgers', 'Sushi']));
-	}
-
-	/**
-	 *
-	 */
-	public function testSelectAndPickSecondOption(): void
+	public function testNonInteractiveSelectAndPickSecondOption(): void
 	{
 		/** @var Input|Mockery\MockInterface $input */
 		$input = Mockery::mock(Input::class);
@@ -126,7 +91,86 @@ class SelectTest extends TestCase
 	/**
 	 *
 	 */
-	public function testSelectAndPickFirstOptionAfterPickingInvalidOption(): void
+	public function testNonInteractiveSelectAndPickMultipleOptions(): void
+	{
+		/** @var Input|Mockery\MockInterface $input */
+		$input = Mockery::mock(Input::class);
+
+		$input->shouldReceive('read')->once()->andReturn('1,2');
+
+		/** @var Environment|Mockery\MockInterface $environment */
+		$environment = Mockery::mock(Environment::class);
+
+		$environment->shouldReceive('hasStty')->once()->andReturn(false);
+
+		/** @var Mockery\MockInterface|Output $output */
+		$output = Mockery::mock(Output::class);
+
+		(function () use ($environment): void {
+			$this->environment = $environment;
+		})->bindTo($output, Output::class)();
+
+		$output->shouldReceive('writeLn')->once()->with('Favorite food?');
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+		1) Burgers
+		2) Sushi
+		>
+		OUTPUT . ' ');
+
+		$select = new Select($input, $output);
+
+		$this->assertSame([0, 1], $select->ask(
+			'Favorite food?',
+			['Burgers', 'Sushi'],
+			allowMultiple: true
+		));
+	}
+
+	/**
+	 *
+	 */
+	public function testNonInteractiveSelectAndPickMultipleOptionsAndReturnValues(): void
+	{
+		/** @var Input|Mockery\MockInterface $input */
+		$input = Mockery::mock(Input::class);
+
+		$input->shouldReceive('read')->once()->andReturn('1,2');
+
+		/** @var Environment|Mockery\MockInterface $environment */
+		$environment = Mockery::mock(Environment::class);
+
+		$environment->shouldReceive('hasStty')->once()->andReturn(false);
+
+		/** @var Mockery\MockInterface|Output $output */
+		$output = Mockery::mock(Output::class);
+
+		(function () use ($environment): void {
+			$this->environment = $environment;
+		})->bindTo($output, Output::class)();
+
+		$output->shouldReceive('writeLn')->once()->with('Favorite food?');
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+		1) Burgers
+		2) Sushi
+		>
+		OUTPUT . ' ');
+
+		$select = new Select($input, $output);
+
+		$this->assertSame(['Burgers', 'Sushi'], $select->ask(
+			'Favorite food?',
+			['Burgers', 'Sushi'],
+			returnKey: false,
+			allowMultiple: true
+		));
+	}
+
+	/**
+	 *
+	 */
+	public function testNonInteractiveSelectAndPickFirstOptionAfterPickingInvalidOption(): void
 	{
 		/** @var Input|Mockery\MockInterface $input */
 		$input = Mockery::mock(Input::class);
@@ -167,5 +211,90 @@ class SelectTest extends TestCase
 		$select = new Select($input, $output);
 
 		$this->assertSame(0, $select->ask('Favorite food?', ['Burgers', 'Sushi']));
+	}
+
+	/**
+	 *
+	 */
+	public function testNonInteractiveSelectAndPickFirstOptionAfterPickingNoOption(): void
+	{
+		/** @var Input|Mockery\MockInterface $input */
+		$input = Mockery::mock(Input::class);
+
+		$input->shouldReceive('read')->once()->andReturn('');
+
+		$input->shouldReceive('read')->once()->andReturn('1');
+
+		/** @var Environment|Mockery\MockInterface $environment */
+		$environment = Mockery::mock(Environment::class);
+
+		$environment->shouldReceive('hasStty')->once()->andReturn(false);
+
+		/** @var Mockery\MockInterface|Output $output */
+		$output = Mockery::mock(Output::class);
+
+		(function () use ($environment): void {
+			$this->environment = $environment;
+		})->bindTo($output, Output::class)();
+
+		$output->shouldReceive('writeLn')->once()->with('Favorite food?');
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+		1) Burgers
+		2) Sushi
+		>
+		OUTPUT . ' ');
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+
+		You need to make a selection.
+
+		1) Burgers
+		2) Sushi
+		>
+		OUTPUT . ' ');
+
+		$select = new Select($input, $output);
+
+		$this->assertSame(0, $select->ask('Favorite food?', ['Burgers', 'Sushi']));
+	}
+
+	/**
+	 *
+	 */
+	public function testNonInteractiveSelectAndNoOption(): void
+	{
+		/** @var Input|Mockery\MockInterface $input */
+		$input = Mockery::mock(Input::class);
+
+		$input->shouldReceive('read')->once()->andReturn('');
+
+		/** @var Environment|Mockery\MockInterface $environment */
+		$environment = Mockery::mock(Environment::class);
+
+		$environment->shouldReceive('hasStty')->once()->andReturn(false);
+
+		/** @var Mockery\MockInterface|Output $output */
+		$output = Mockery::mock(Output::class);
+
+		(function () use ($environment): void {
+			$this->environment = $environment;
+		})->bindTo($output, Output::class)();
+
+		$output->shouldReceive('writeLn')->once()->with('Favorite food?');
+
+		$output->shouldReceive('write')->once()->with(<<<'OUTPUT'
+		1) Burgers
+		2) Sushi
+		>
+		OUTPUT . ' ');
+
+		$select = new Select($input, $output);
+
+		$this->assertSame(null, $select->ask(
+			'Favorite food?',
+			['Burgers', 'Sushi'],
+			allowEmptySelection: true
+		));
 	}
 }
