03a587aa059ac9d3d089d4d140eb92200a513a4c
Frederic G. Østby <frederic.g.ostby@gmail.com>
2025-08-25 19:40:29 +0200

    Added InjectConnection attribute for Redis connections

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@
 
 * The container can now resolve parameters with the help of custom injector attributes. The following attributes are included by default:
 	- `InjectConnection` which allows you to inject a database connection.
+	- `InjectConnection` which allows you to inject a Redis connection.
 	- `InjectConfig` which allows you to inject a config value.
 	- `InjectEnv` which allows you to inject a environment variable value.
 * Added `disableAfterSunset` option to the `Deprecated` middleware.
---
+++ src/mako/redis/attributes/syringe/InjectConnection.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @copyright Frederic G. Østby
+ * @license   http://www.makoframework.com/license
+ */
+
+namespace mako\redis\attributes\syringe;
+
+use Attribute;
+use mako\redis\ConnectionManager;
+use mako\redis\Redis;
+use mako\syringe\attributes\InjectorInterface;
+use Override;
+use ReflectionParameter;
+
+/**
+ * Connection injector.
+ */
+#[Attribute(Attribute::TARGET_PARAMETER)]
+class InjectConnection implements InjectorInterface
+{
+	/**
+	 * Constructor.
+	 */
+	public function __construct(
+		protected ?string $connection,
+		protected ConnectionManager $connectionManager
+	) {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	#[Override]
+    public function getParameterValue(ReflectionParameter $parameter): Redis
+    {
+		return $this->connectionManager->getConnection($this->connection);
+	}
+}
