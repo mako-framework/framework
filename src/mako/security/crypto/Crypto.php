27b019523592e407c19aef94b5e56c306473ed6d
Frederic G. Østby <frederic.g.ostby@gmail.com>
2025-08-26 22:33:03 +0200

    Update Crypto.php

--- src/mako/security/crypto/Crypto.php
+++ src/mako/security/crypto/Crypto.php
@@ -21,7 +21,7 @@ class Crypto
 	 * Constructor.
 	 */
 	public function __construct(
-		public protected(set) EncrypterInterface $adapter,
+		public protected(set) EncrypterInterface $encrypter,
 		public protected(set) Signer $signer
 	) {
 	}
@@ -31,7 +31,7 @@ class Crypto
 	 */
 	public function encrypt(#[SensitiveParameter] string $string): string
 	{
-		return $this->signer->sign($this->adapter->encrypt($string));
+		return $this->signer->sign($this->encrypter->encrypt($string));
 	}
 
 	/**
@@ -45,6 +45,6 @@ class Crypto
 			throw new CryptoException('Ciphertex has been modified or an invalid authentication key has been provided.');
 		}
 
-		return $this->adapter->decrypt($string);
+		return $this->encrypter->decrypt($string);
 	}
 }
