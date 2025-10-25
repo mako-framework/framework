9db87a8df805d7d1ff0c6d7d4aad14f32b5a219e
Frederic G. Østby <frederic.g.ostby@gmail.com>
2025-10-25 11:08:41 +0200

    Update 429.tpl.php

--- src/mako/error/handlers/web/views/429.tpl.php
+++ src/mako/error/handlers/web/views/429.tpl.php
@@ -2,7 +2,44 @@
 
 {% block:'title' %}Too Many Requests{% endblock %}
 
+<?php
+
+use mako\http\exceptions\TooManyRequestsException;
+
+if (isset($_exception_) && $_exception_ instanceof TooManyRequestsException) {
+	/** @var TooManyRequestsException $_exception_ */
+	$retryAfter = $_exception_->getRetryAfter();
+
+	if (
+		$retryAfter !== null
+		&& $retryAfter->getTimezone()->getName() !== 'UTC'
+		&& $retryAfter instanceof DateTime || $retryAfter instanceof DateTimeImmutable
+	) {
+		$retryAfter->setTimezone(new DateTimeZone('UTC'));
+		$retryAfter = $retryAfter->format('D, d M Y H:i:s \G\M\T');
+	}
+}
+
+?>
+
 {% block:'message' %}
 	<h1>429</h1>
-	<p>You have made too many requests to the server.</p>
+	<p>
+		You have made too many requests to the server.
+		<span id="retryAt" hidden aria-hidden="true">
+			<i>You can retry at <span id="retryDate" data-date="{{$retryAfter, default: ''}}"></span></i>.
+		</span>
+	</p>
+	<script type="text/javascript">
+		const retryDate = document.getElementById('retryDate');
+		const retryUtcDate = retryDate.dataset.date;
+
+		if (retryUtcDate) {
+			retryDate.textContent = (new Date(retryUtcDate)).toLocaleString();
+
+			const retryAt = document.getElementById('retryAt');
+			retryAt.hidden = false;
+			retryAt.removeAttribute('aria-hidden');
+		}
+	</script>
 {% endblock %}
