7505868bb8e6324d19e8b95e2c30df1c78bbb898
Frederic G. Østby <frederic.g.ostby@gmail.com>
2026-01-09 03:13:59 +0100

    Update Mako.php

--- src/mako/Mako.php
+++ src/mako/Mako.php
@@ -15,7 +15,7 @@ final class Mako
 	/**
 	 * Mako version.
 	 */
-	public const string VERSION = '11.4.4';
+	public const string VERSION = '11.4.6';
 
 	/**
 	 * Mako major version.
@@ -30,5 +30,5 @@ final class Mako
 	/**
 	 * Mako patch version.
 	 */
-	public const int VERSION_PATCH = 4;
+	public const int VERSION_PATCH = 6;
 }
