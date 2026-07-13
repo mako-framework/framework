a7bc1bba292360764b5221e6e22555629da31463
Frederic G. Østby <frederic.g.ostby@gmail.com>
2026-07-13 20:11:03 +0200

    Update CustomStatus.php

--- src/mako/http/response/CustomStatus.php
+++ src/mako/http/response/CustomStatus.php
@@ -18,26 +18,26 @@ final class CustomStatus implements StatusInterface
 {
 	use StatusTrait;
 
-    public int $value { get => $this->statusCode; }
+	public int $value { get => $this->statusCode; }
 
-    /**
-     * Constructor.
-     */
-    public function __construct(
-        protected readonly int $statusCode,
-        protected readonly string $statusText
-    ) {
+	/**
+	 * Constructor.
+	 */
+	public function __construct(
+		protected readonly int $statusCode,
+	protected readonly string $statusText
+	) {
 		if ($statusCode < 100 || $statusCode > 599) {
 			throw new InvalidArgumentException('The status code must be between 100 and 599.');
 		}
-    }
+	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	#[Override]
-    public function getMessage(): string
-    {
-        return $this->statusText;
-    }
+	public function getMessage(): string
+	{
+		return $this->statusText;
+	}
 }
