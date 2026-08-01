974bbfbdbacfcbc56e74579f39dd2918796dc287
Frederic G. Østby <frederic.g.ostby@gmail.com>
2026-08-01 23:08:02 +0200

    Added position calculation helpers

---
+++ src/mako/pixel/image/operations/Position.php
@@ -0,0 +1,151 @@
+<?php
+
+/**
+ * @copyright Frederic G. Østby
+ * @license   http://www.makoframework.com/license
+ */
+
+namespace mako\pixel\image\operations;
+
+use mako\pixel\image\Dimensions;
+
+/**
+ * Position calculation helpers.
+ *
+ * Provides methods for calculating the top-left coordinates required to place
+ * objects within a container. The returned positions can be used as offsets
+ * when drawing or placing objects on an image.
+ */
+final class Position
+{
+	/**
+	 * Returns the position required to place an object at the top-left corner
+	 * of a container.
+	 *
+	 * The returned point represents the top-left coordinate where the object
+	 * should be placed.
+	 */
+	public static function topLeft(Dimensions $container, Dimensions $object, int $padding = 0): Point
+	{
+		return new Point($padding, $padding);
+	}
+
+	/**
+	 * Returns the position required to place an object at the top center
+	 * of a container.
+	 *
+	 * The returned point represents the top-left coordinate where the object
+	 * should be placed.
+	 */
+	public static function top(Dimensions $container, Dimensions $object, int $padding = 0): Point
+	{
+		return new Point(
+			(int) (($container->width - $object->width) / 2),
+			$padding,
+		);
+	}
+
+	/**
+	 * Returns the position required to center an object inside a container.
+	 *
+	 * The returned point represents the top-left coordinate where the object
+	 * should be placed.
+	 */
+	public static function center(Dimensions $container, Dimensions $object): Point
+	{
+		return new Point(
+			(int) (($container->width - $object->width) / 2),
+			(int) (($container->height - $object->height) / 2),
+		);
+	}
+
+	/**
+	 * Returns the position required to place an object at the top-right corner
+	 * of a container.
+	 *
+	 * The returned point represents the top-left coordinate where the object
+	 * should be placed.
+	 */
+	public static function topRight(Dimensions $container, Dimensions $object, int $padding = 0): Point
+	{
+		return new Point(
+			$container->width - $object->width - $padding,
+			$padding,
+		);
+	}
+
+	/**
+	 * Returns the position required to place an object at the center-left
+	 * of a container.
+	 *
+	 * The returned point represents the top-left coordinate where the object
+	 * should be placed.
+	 */
+	public static function centerLeft(Dimensions $container, Dimensions $object, int $padding = 0): Point
+	{
+		return new Point(
+			$padding,
+			(int) (($container->height - $object->height) / 2),
+		);
+	}
+
+	/**
+	 * Returns the position required to place an object at the center-right
+	 * of a container.
+	 *
+	 * The returned point represents the top-left coordinate where the object
+	 * should be placed.
+	 */
+	public static function centerRight(Dimensions $container, Dimensions $object, int $padding = 0): Point
+	{
+		return new Point(
+			$container->width - $object->width - $padding,
+			(int) (($container->height - $object->height) / 2),
+		);
+	}
+
+	/**
+	 * Returns the position required to place an object at the bottom-left
+	 * corner of a container.
+	 *
+	 * The returned point represents the top-left coordinate where the object
+	 * should be placed.
+	 */
+	public static function bottomLeft(Dimensions $container, Dimensions $object, int $padding = 0): Point
+	{
+		return new Point(
+			$padding,
+			$container->height - $object->height - $padding,
+		);
+	}
+
+	/**
+	 * Returns the position required to place an object at the bottom center
+	 * of a container.
+	 *
+	 * The returned point represents the top-left coordinate where the object
+	 * should be placed.
+	 */
+	public static function bottom(Dimensions $container, Dimensions $object, int $padding = 0): Point
+	{
+		return new Point(
+			(int) (($container->width - $object->width) / 2),
+			$container->height - $object->height - $padding,
+		);
+	}
+
+	/**
+	 * Returns the position required to place an object at the bottom-right
+	 * corner of a container.
+	 *
+	 * The returned point represents the top-left coordinate where the object
+	 * should be placed.
+	 */
+	public static function bottomRight(Dimensions $container, Dimensions $object, int $padding = 0): Point
+	{
+		return new Point(
+			$container->width - $object->width - $padding,
+			$container->height - $object->height - $padding,
+		);
+	}
+}
