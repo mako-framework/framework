76e3ea312772be58545a8a69ed6fa1ed5cdd246f
Frederic G. Østby <frederic.g.ostby@gmail.com>
2026-08-08 19:28:38 +0200

    Cleaning up

--- tests/unit/pixel/image/operations/imagemagick/OrientTest.php
+++ tests/unit/pixel/image/operations/imagemagick/OrientTest.php
@@ -27,15 +27,78 @@ class OrientTest extends TestCase
 	{
 		$files = glob(__DIR__ . '/../../fixtures/orientation/test_*.jpg');
 
-		foreach ($files as $file) {
+		natsort($files);
+
+		$expectedBeforeOrient = [
+			[
+				'tl' => ColorFamily::Red,
+				'tr' => ColorFamily::Green,
+				'bl' => ColorFamily::Blue,
+				'br' => ColorFamily::White,
+			],
+			[
+				'tl' => ColorFamily::Green,
+				'tr' => ColorFamily::Red,
+				'bl' => ColorFamily::White,
+				'br' => ColorFamily::Blue,
+			],
+			[
+				'tl' => ColorFamily::White,
+				'tr' => ColorFamily::Blue,
+				'bl' => ColorFamily::Green,
+				'br' => ColorFamily::Red,
+			],
+			[
+				'tl' => ColorFamily::Blue,
+				'tr' => ColorFamily::White,
+				'bl' => ColorFamily::Red,
+				'br' => ColorFamily::Green,
+			],
+			[
+				'tl' => ColorFamily::Red,
+				'tr' => ColorFamily::Blue,
+				'bl' => ColorFamily::Green,
+				'br' => ColorFamily::White,
+			],
+			[
+				'tl' => ColorFamily::Green,
+				'tr' => ColorFamily::White,
+				'bl' => ColorFamily::Red,
+				'br' => ColorFamily::Blue,
+			],
+			[
+				'tl' => ColorFamily::White,
+				'tr' => ColorFamily::Green,
+				'bl' => ColorFamily::Blue,
+				'br' => ColorFamily::Red,
+			],
+			[
+				'tl' => ColorFamily::Blue,
+				'tr' => ColorFamily::Red,
+				'bl' => ColorFamily::White,
+				'br' => ColorFamily::Green,
+			],
+		];
+
+		foreach ($files as $key => $file) {
 			$image = new ImageMagick($file);
+
+			$colorTl = $image->inspect(new ColorAt(new Point(2, 2)));
+			$colorTr = $image->inspect(new ColorAt(new Point(48, 2)));
+			$colorBl = $image->inspect(new ColorAt(new Point(2, 48)));
+			$colorBr = $image->inspect(new ColorAt(new Point(48, 48)));
+
+			$this->assertSame($expectedBeforeOrient[$key]['tl'], $colorTl->toColorFamily(), $file);
+			$this->assertSame($expectedBeforeOrient[$key]['tr'], $colorTr->toColorFamily(), $file);
+			$this->assertSame($expectedBeforeOrient[$key]['bl'], $colorBl->toColorFamily(), $file);
+			$this->assertSame($expectedBeforeOrient[$key]['br'], $colorBr->toColorFamily(), $file);
 
 			$image->apply(new Orient);
 
 			$colorTl = $image->inspect(new ColorAt(new Point(2, 2)));
 			$colorTr = $image->inspect(new ColorAt(new Point(48, 2)));
-			$colorBl = $image->inspect(new ColorAt(new Point(2, 98)));
-			$colorBr = $image->inspect(new ColorAt(new Point(48, 98)));
+			$colorBl = $image->inspect(new ColorAt(new Point(2, 48)));
+			$colorBr = $image->inspect(new ColorAt(new Point(48, 48)));
 
 			$this->assertSame(ColorFamily::Red, $colorTl->toColorFamily(), $file);
 			$this->assertSame(ColorFamily::Green, $colorTr->toColorFamily(), $file);
