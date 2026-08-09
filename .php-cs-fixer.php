a5f7e5023f8599705b055a27d5cedb2e9c5839be
Frederic G. Østby <frederic.g.ostby@gmail.com>
2026-08-09 09:54:15 +0200

    no_useless_printf

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -73,6 +73,7 @@ return (new Config)->setRules
 	'no_unset_cast' => true,
 	'no_unused_imports' => true,
 	'no_useless_concat_operator' => true,
+	'no_useless_printf' => true,
 	'no_useless_sprintf' => true,
 	'no_whitespace_before_comma_in_array' => true,
 	'no_whitespace_in_blank_line' => true,
