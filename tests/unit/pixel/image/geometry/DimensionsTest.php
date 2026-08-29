06ce37b071ed750e2bd5d336e8a84fd8aa5e9857
Frederic G. Østby <frederic.g.ostby@gmail.com>
2026-08-29 11:01:44 +0200

    Create DimensionsTest.php

---
+++ tests/unit/pixel/image/geometry/DimensionsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @copyright Frederic G. Østby
+ * @license   http://www.makoframework.com/license
+ */
+
+namespace mako\tests\unit\pixel\image\geometry;
+
+use mako\pixel\image\geometry\Dimensions;
+use mako\tests\TestCase;
+use PHPUnit\Framework\Attributes\Group;
+
+#[Group('unit')]
+class DimensionsTest extends TestCase
+{
+	/**
+	 *
+	 */
+	public function testDimensions(): void
+	{
+		$dimensions = new Dimensions(50, 100);
+
+		$this->assertSame(50, $dimensions->width);
+		$this->assertSame(100, $dimensions->height);
+	}
+}
