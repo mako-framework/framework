36a152d11d5d1e26e8224551ec55716c7afbbe29
Frederic G. Østby <frederic.g.ostby@gmail.com>
2026-08-31 17:49:27 +0200

    Updated invert test

--- tests/unit/pixel/image/operations/imagemagick/InvertTest.php
+++ tests/unit/pixel/image/operations/imagemagick/InvertTest.php
@@ -7,11 +7,14 @@
 
 namespace mako\tests\unit\pixel\image\operations\imagemagick;
 
+use mako\pixel\image\Color;
+use mako\pixel\image\geometry\Dimensions;
+use mako\pixel\image\geometry\Point;
 use mako\pixel\image\ImageMagick;
-use mako\pixel\image\inspectors\imagemagick\TopColors;
-use mako\pixel\image\operations\imagemagick\Bitonal;
+use mako\pixel\image\inspectors\imagemagick\ColorAt;
 use mako\pixel\image\operations\imagemagick\Invert;
 use mako\tests\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\RequiresPhpExtension;
 
@@ -20,20 +23,43 @@ use PHPUnit\Framework\Attributes\RequiresPhpExtension;
 class InvertTest extends TestCase
 {
 	/**
+	 * Returns [original color, expected inverted color].
+	 */
+	public static function colorProvider(): array
+	{
+		return [
+			'white'    => [new Color(255, 255, 255), '#000000'],
+			'black'    => [new Color(0, 0, 0), '#FFFFFF'],
+			'red'      => [new Color(255, 0, 0), '#00FFFF'],
+			'green'    => [new Color(0, 255, 0), '#FF00FF'],
+			'blue'     => [new Color(0, 0, 255), '#FFFF00'],
+			'mid gray' => [new Color(100, 150, 200), '#9B6937'],
+		];
+	}
+
+	/**
 	 *
 	 */
-	public function testNegate(): void
+	#[DataProvider('colorProvider')]
+	public function testInvert(Color $color, string $expectedColor): void
 	{
-		$image = new ImageMagick(__DIR__ . '/../../fixtures/002.jpg');
+		$image = ImageMagick::create(new Dimensions(1, 1), $color);
 
-		$image->apply(new Bitonal);
 		$image->apply(new Invert);
 
-		$colors = $image->inspect(new TopColors);
+		$this->assertSame($expectedColor, $image->inspect(new ColorAt(new Point(0, 0)))->toHexString());
+	}
 
-		$this->assertSame(2, count($colors));
+	/**
+	 *
+	 */
+	public function testDoubleInvertRestoresOriginalColor(): void
+	{
+		$image = ImageMagick::create(new Dimensions(1, 1), new Color(100, 150, 200));
 
-		$this->assertSame('#000000', $colors[0]->toHexString());
-		$this->assertSame('#FFFFFF', $colors[1]->toHexString());
+		$image->apply(new Invert);
+		$image->apply(new Invert);
+
+		$this->assertSame('#6496C8', $image->inspect(new ColorAt(new Point(0, 0)))->toHexString());
 	}
 }
